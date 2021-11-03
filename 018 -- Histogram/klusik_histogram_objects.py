19f70e9900fdcc4d7a00452c88e3c3c03ca44bf0
Rudolf Klusal <klusik@klusik.cz>
2021-11-03 23:35:08 +0100

    something I forgot to do

---
+++ 018 -- Histogram/klusik_histogram_objects.py
@@ -0,0 +1,18 @@
+
+from urllib.request import urlopen
+
+class Histogram:
+    def __init__(self):
+        self.histogram = dict()
+
+    def add(self):
+        pass
+
+    def sort(self):
+        pass
+
+    def view(self):
+        pass
+
+    def readWeb(self):
+        pass
