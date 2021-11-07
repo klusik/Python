d03deb2c7abaea67b6adf53ca910c63dbc8c9f94
Rudolf Klusal <klusik@klusik.cz>
2021-11-07 15:01:15 +0100

    Classes, basic config

--- Bonusy/Ecosystem/ecosystem.py
+++ Bonusy/Ecosystem/ecosystem.py
@@ -1,7 +1,13 @@
 """Simulation of an ecosystem"""
 
+# IMPORTS
+import logging
+
+# CLASSES
+
 class Ecosystem:
-    pass
+    def __init__(self):
+        pass
 
 class Herbivore:
     pass
@@ -15,3 +21,18 @@ class Predator:
 class Human:
     pass
 
+# CONFIG
+logging.basicConfig(level=logging.INFO)
+
+# RUNTIME
+def main():
+    try:
+        # Creating an ecosystem playground
+        ecosystem = Ecosystem()
+    except Exception as exception:
+        logging.info(f"Ecosystem failed:\n{exception}")
+    finally:
+        print("That's all :-)")
+
+if __name__ == "__main__":
+    main()
