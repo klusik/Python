135dc7422faa269411dfa18f2a9a79584f4478db
Rudolf Klusal <klusik@klusik.cz>
2021-11-07 16:21:27 +0100

    Happiness, sex

--- Bonusy/Ecosystem/ecosystem.py
+++ Bonusy/Ecosystem/ecosystem.py
@@ -2,7 +2,9 @@
 
 # IMPORTS
 import logging
+import random
 
+from enum import Enum
 
 # CLASSES
 class Defaults:
@@ -12,18 +14,25 @@ class Defaults:
 
     # Number of trees
     trees = 100
+    treeMaxAge = 100 * 365 # 100 years
+
 
     # Number of humans
     humans = 100
+    humanMaxAge = 71 * 365 # 71 years
 
     # Number of omnivores
     omnivores = 100
+    omnivoreMaxAge = 30 * 365 # 30 years
 
     # Number of herbivores
     herbivores = 100
+    herbivoreMaxAge = 30 * 365 # 30 years
 
     # Number of predators
     predators = 100
+    predatorMaxAge = 20 * 360 # 20 years
+
 
 
 class Ecosystem:
@@ -37,6 +46,7 @@ class Ecosystem:
         if not len(self.idSet):
             # Empty set
             self.idSet.add(1)
+            return 1
 
         else:
             # Not empty set
@@ -51,42 +61,96 @@ class Ecosystem:
                     return newId
                     break
 
-    def getTrees(self):
-        for tree in self.trees:
-            print(f"Tree: Id = {tree.treeId}")
+    def getTrees(self, treeId = None):
+        if treeId:
+            print(f"Tree with id = {treeId}")
+        else:
+            for tree in self.trees:
+                print(f"Tree: Id = {str(tree.treeId):5} | "
+                        f"Happiness: {str(tree.happiness):1.3} | "
+                        f"Age: {str(tree.age):3} | "
+                        f"Sex: {str(sex(tree.sex).name):7}")
 
 
-    def __init__(self, initialState = None, trees = Defaults.trees):
+    def __init__(self, initialState = None,
+                 trees = Defaults.trees,
+                 herbivores = Defaults.herbivores,
+                 omnivores = Defaults.omnivores,
+                 predators = Defaults.predators,
+                 humans = Defaults.humans):
         try:
+            # Generate trees
             self.trees = list()
             for tree in range(trees):
                 newTreeId = self.generateId()
                 self.trees.append(Tree(treeId = newTreeId))
 
+            # Generate herbivores
+            self.herbivores = list()
+            for herbivore in range(herbivores):
+                newHerbivoreId = self.generateId()
+                self.herbivores.append(Herbivore(herbivoreId = newHerbivoreId))
+
+            # Generate omnivores
+            self.omnivores = list()
+            for omnivore in range(omnivores):
+                self.omnivores.append(Omnivore(omnivoreId = self.generateId()))
+
+            # Generate predators
+            self.predators = list()
+            for predator in range(predators):
+                self.predators.append(Predator(predatorId = self.generateId()))
+
+            # Generate humans
+            self.humans = list()
+            for human in range(humans):
+                self.humans.append(Human(humanId = self.generateId()))
+
         except Exception as exception:
             raise exception
 
 
+class sex(Enum):
+    MALE = 1
+    FEMALE = 2
 
-class Tree:
+
+class Being():
+    """Default class for every being in Ecosystem"""
+
+    # Happiness ranges from 0 to 1, 0 totally sad, 1 totally happy
+    happiness = 1.0
+    age = 0
+    sex = 0
+
+class Tree(Being):
     def __init__(self, treeId):
         self.treeId = treeId
+        self.sex = random.randrange(1, 3, 1)
 
 
-class Herbivore:
-    pass
+class Herbivore(Being):
+    def __init__(self, herbivoreId):
+        self.herbivoreId = herbivoreId
+        self.sex = random.randrange(1, 3, 1)
 
 
-class Omnivore:
-    pass
+class Omnivore(Being):
+    def __init__(self, omnivoreId):
+        self.omnivoreId = omnivoreId
+        self.sex = random.randrange(1, 3, 1)
 
 
-class Predator:
-    pass
+class Predator(Being):
+    def __init__(self, predatorId):
+        self.predatorId = predatorId
+        self.sex = random.randrange(1, 3, 1)
 
 
-class Human:
-    pass
+class Human(Being):
+    def __init__(self, humanId):
+        self.humanId = humanId
+        self.sex = random.randrange(1, 3, 1)
 
 
 # CONFIG
@@ -99,6 +163,7 @@ def main():
         # Creating an ecosystem playground
         ecosystem = Ecosystem()
         ecosystem.getTrees()
+
 
     except TypeError:
         logging.INFO("e")
