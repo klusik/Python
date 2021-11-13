bc175d97d7094e026ce47a161d7981cc0f7e49d7
Rudolf Klusal <klusik@klusik.cz>
2021-11-14 00:20:21 +0100

    Initial comment

---
+++ Danny/convert_numbers_to_words.py
@@ -0,0 +1,13 @@
+# Program for rewriting a number from
+# a text to the number.
+#
+# Input is limited from "one" to "five",
+# letters only lowercase (Python is case
+# sensitive).
+#
+# All lines are heavily commented, so I hope
+# you'll understand everything
+#
+# Author:   klusik@klusik.cz
+#           2021
+#
