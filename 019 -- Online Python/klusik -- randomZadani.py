6a9c8b3f72e2ce1361bcdef02c201f508eeab79f
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2021-12-15 13:38:02 +0100

    Zeros counting

--- 019 -- Online Python/klusik -- randomZadani.py
+++ 019 -- Online Python/klusik -- randomZadani.py
@@ -4,3 +4,33 @@
 # preserving the order of the other elements.
 #
 
+# IMPORTS
+import random
+
+# CONFIG
+digitsCount = 10
+maxNumber = 4
+minNumber = 0
+
+# PROGRAM
+def main():
+    listOfNumbers = list()
+
+    for index in range(digitsCount):
+        listOfNumbers.append(random.randrange(minNumber, maxNumber+1))
+
+    print("Original list: ")
+    print(listOfNumbers)
+
+    zeroCount = 0
+    for digit in listOfNumbers:
+        if digit == 0:
+            zeroCount += 1
+
+    if zeroCount == 0:
+        print("List doesn't contain any zero.")
+    else:
+        print(f"List contains {zeroCount} zeros.")
+        
+if __name__ == "__main__":
+    main()
