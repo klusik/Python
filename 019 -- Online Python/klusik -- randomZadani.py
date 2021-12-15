6d6054d607a2b693e5dc8a9ead96fa6cbf0ed66f
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2021-12-15 13:52:01 +0100

    Final push

--- 019 -- Online Python/klusik -- randomZadani.py
+++ 019 -- Online Python/klusik -- randomZadani.py
@@ -12,25 +12,44 @@ digitsCount = 10
 maxNumber = 4
 minNumber = 0
 
+
 # PROGRAM
 def main():
     listOfNumbers = list()
 
+    # Creating a list
     for index in range(digitsCount):
         listOfNumbers.append(random.randrange(minNumber, maxNumber+1))
 
+    # Displaying an original list
     print("Original list: ")
     print(listOfNumbers)
 
+    # Counting zeroes and removing them.
     zeroCount = 0
-    for digit in listOfNumbers:
+    for digitIndex, digit in enumerate(listOfNumbers):
         if digit == 0:
             zeroCount += 1
 
+
+    # Displaying a number of zeroes
     if zeroCount == 0:
         print("List doesn't contain any zero.")
     else:
         print(f"List contains {zeroCount} zeros.")
-        
+
+    # Removing zeros
+    for _ in range(zeroCount):
+        listOfNumbers.remove(0)
+
+    # Adding zeroes to the end
+    if zeroCount > 0:
+        for _ in range(zeroCount):
+            listOfNumbers.append(0)
+
+    # Displaying final list
+    print("After zero moving: ")
+    print(listOfNumbers)
+
 if __name__ == "__main__":
     main()
