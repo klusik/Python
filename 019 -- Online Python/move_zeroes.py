03c3106922b6d0717090774df867ea809ea33b4b
kopfrkingl <sykora.michal1@gmail.com>
2021-12-15 15:42:23 +0100

    random changes

--- 019 -- Online Python/move_zeroes.py
+++ 019 -- Online Python/move_zeroes.py
@@ -6,11 +6,17 @@ import random
 
 def reconfigurator(policko):
     nullcounter = 0
-    # removing zeroes from the position and counting volume of zeroes
+
+    # calculate number of zeroes
+    for i in policko:
+        if i == 0:
+            nullcounter += 1
+
+    # removing zeroes from the positions
     for i in policko:
         if i == 0:
             policko.remove(0)
-            nullcounter += 1
+            # nullcounter += 1
     
     # adding zeroes to the end of the list
     for i in range(nullcounter):
@@ -23,5 +29,6 @@ start = 0
 stop = 3
 count = 10
 lRan = [random.randint(start,stop) for i in range(count)]
+
 print(lRan)
 print(reconfigurator([lRan]))
