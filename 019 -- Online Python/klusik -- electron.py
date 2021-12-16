c6b6d1856a2640a00f73c60366ed4a44c3194247
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2021-12-16 13:16:31 +0100

    User input

---
+++ 019 -- Online Python/klusik -- electron.py
@@ -0,0 +1,17 @@
+#
+# CW: https://www.codewars.com/kata/59175441e76dc9f9bc00000f
+#
+
+def main():
+    # Let a user enter a value:
+
+    atomicNumber = round(int(input("Enter the atomic number (will be rounded): ")))
+
+    if atomicNumber < 1:
+        atomicNumber = 1
+
+
+
+
+if __name__ == "__main__":
+    main()
