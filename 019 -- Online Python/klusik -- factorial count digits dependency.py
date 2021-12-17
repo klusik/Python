66a10f883d7fdb18f77680eade05fa5dfd0734e9
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2021-12-17 11:14:04 +0100

    Default framework

---
+++ 019 -- Online Python/klusik -- factorial count digits dependency.py
@@ -0,0 +1,13 @@
+# Create a list of tuplets in format
+# [(n, length(n!)), ...]
+#
+# This could be exported to excel or gnuplot
+
+
+import numpy
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
