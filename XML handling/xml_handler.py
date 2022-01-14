ac298b828c13ad62de6093f380e641314ddc9a38
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2022-01-14 09:11:12 +0100

    XML write file, indented

--- XML handling/xml_handler.py
+++ XML handling/xml_handler.py
@@ -4,19 +4,32 @@
 #
 
 # IMPORTS #
-
+import os
+import glob
 import xml.etree.ElementTree as ElementTree
+
+# FUNCTIONS #
+def readAllFiles():
+    path = "../**"
+    structure = glob.glob(path, recursive=True)
+    return structure
 
 # RUNTIME
 def main():
     """Main function"""
     root = ElementTree.Element("root")
-    something = ElementTree.SubElement(root, "Something")
+    something = ElementTree.SubElement(root, "something")
 
-    ElementTree.SubElement(something, "Aha, takhle", name="description").text = "Tohe je vnitrni hodnota."
+    ElementTree.SubElement(something, "innerSomething", name="description").text = "Tohe je vnitrni hodnota."
 
+    ElementTree.indent(root)
     xmlTree = ElementTree.ElementTree(root)
     xmlTree.write("file.xml")
-    
+
+    fileStructure = readAllFiles()
+    print(f"Found {len(fileStructure)} files.")
+    for file in fileStructure:
+        print(file)
+
 if __name__ == "__main__":
     main()
