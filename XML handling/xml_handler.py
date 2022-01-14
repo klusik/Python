9f097861b1da2ce192af6d2730c6227e5b3ca1ad
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2022-01-14 08:47:05 +0100

    XML create

--- XML handling/xml_handler.py
+++ XML handling/xml_handler.py
@@ -4,3 +4,19 @@
 #
 
 # IMPORTS #
+
+import xml.etree.ElementTree as ElementTree
+
+# RUNTIME
+def main():
+    """Main function"""
+    root = ElementTree.Element("root")
+    something = ElementTree.SubElement(root, "Something")
+
+    ElementTree.SubElement(something, "Aha, takhle", name="description").text = "Tohe je vnitrni hodnota."
+
+    xmlTree = ElementTree.ElementTree(root)
+    xmlTree.write("file.xml")
+    
+if __name__ == "__main__":
+    main()
