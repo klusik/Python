bdc7e919a7671b4047e61d04ad259686765cf5fe
Rudolf Klusal <klusik@klusik.cz>
2022-01-19 19:43:18 +0100

    Default user friendliness :-)

--- Bonusy/pythonProject/klusik -- noty.py
+++ Bonusy/pythonProject/klusik -- noty.py
@@ -1,8 +1,19 @@
 # Codewars -- notes
 
+# FUNCTIONS #
+def toneShift(tone):
+    """Returns a relative pitch shift from 'a' of given tone 'tone' """
+
 # RUNTIME #
 def main():
     """ Main function """
+
+    while True:
+        tone = str(input("Input any note in format TO, where T is tone, O is octave, "
+                         "e.g. A#3 means A sharp, 3rd octave. Enter 'bye' for exit: "))
+
+        if tone == "bye":
+            break
 
 if __name__ == "__main__":
     main()
