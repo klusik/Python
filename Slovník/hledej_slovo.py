6907b5d8d014804ef10419ce5853470ac28ae6f0
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2022-01-21 13:24:05 +0100

    Basic classes

--- Slovník/hledej_slovo.py
+++ Slovník/hledej_slovo.py
@@ -8,10 +8,27 @@
 # IMPORTS #
 
 # CLASSES #
+class Config:
+    """ Defaults """
+    dictionary_path = "cs_CZ.dic"
+
+    
+class Dictionary:
+    """Class contains the dictionary stuff """
+    def init(self):
+
+        # ATTRIBUTES #
+        self.dictionary = dict()    # Contains all words
+
+
+    def load_dictionary_from_file(self):
+        """ Populates a dictionary from a default file """
+
+
 
 # RUNTIME #
 def main():
     """ Main runtime"""
-    
+
 if __name__ == "__main__":
     main()
