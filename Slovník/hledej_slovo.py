7c7508dbeabec06a2d7e93469e6e50b9dd70d30b
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2022-01-21 13:20:26 +0100

    Basic structure (initial commit) -- Slovnik.

---
+++ Slovník/hledej_slovo.py
@@ -0,0 +1,17 @@
+""" Search for a word in Czech dictionary
+    Seach is based on a letters given as an input
+    by user (e.g. 'klsuta' will make words like 'klus', 'lusk' etc.)
+
+
+"""
+
+# IMPORTS #
+
+# CLASSES #
+
+# RUNTIME #
+def main():
+    """ Main runtime"""
+    
+if __name__ == "__main__":
+    main()
