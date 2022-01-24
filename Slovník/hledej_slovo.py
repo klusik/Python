b5504ec1abcb7c004456f072d326528138385264
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2022-01-24 08:33:37 +0100

    Adding words to memory.

--- Slovník/hledej_slovo.py
+++ Slovník/hledej_slovo.py
@@ -13,15 +13,37 @@ class Config:
     dictionary_path = "cs_CZ.dic"
 
 
+class Word:
+    def __init__(self, word):
+        self.word = word
+
+    def is_real_word(self):
+        """ If the word is a real word or just a heading or something """
+
+        # If the given word is only a digit, it's not a word
+        if str(self.word).isdigit():
+            return False
+
+        # If the word contains ONLY capital letters (not any, ONLY),
+        # then it's not a word as well.
+        if str(self.word).upper() == str(self.word):
+            return False
+
+
+        return True
+
+    def get_word(self):
+        """ Words could contain a forward slash and grammar case and other stuff
+            in that, so if that's the case, return just a word before that. """
+        return str(self.word).split("/")[0]
+
 class Dictionary:
     """Class contains the dictionary stuff """
-    def init(self):
+    def __init__(self):
 
         # ATTRIBUTES #
         self.dictionary = dict()    # Contains all words
 
-    def is_real_word(self, word):
-        pass
 
     def load_dictionary_from_file(self,
                                   file = Config.dictionary_path):
@@ -46,11 +68,16 @@ class Dictionary:
         # Some words contains only CAPITALLETTERS, these are not words,
         # these are headers of sections, like 'ABB'
 
-        for word in list_of_all_words_from_file:
+        all_words = set()
 
+        for line_from_file in list_of_all_words_from_file:
+            word = Word(line_from_file)
 
+            if word.is_real_word():
+                all_words.add(word.get_word())
 
-
+            # Clear the memory
+            del(word)
 
 # RUNTIME #
 def main():
