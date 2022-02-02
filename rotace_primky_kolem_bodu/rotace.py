79a8ec2c918149b3a2e5fbc4505e827ad632cecf
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-02 09:08:45 +0100

    Basic code structure

---
+++ rotace_primky_kolem_bodu/rotace.py
@@ -0,0 +1,31 @@
+"""
+    Rotate an abscisse around a point
+
+    Author:     Rudolf Klusal
+    Year:       2022
+
+"""
+
+# IMPORTS #
+
+# CLASSES #
+class Abscisse:
+    def __init__(self,
+                 x, # a touple with x coordinates
+                 y, # a touple with y coordinates
+                 ):
+        """ Init method """
+        self.x = x
+        self.y = y
+
+    def get_length(self):
+        """ Returns a length of an abscisse """
+        pass
+
+
+# RUNTIME #
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
