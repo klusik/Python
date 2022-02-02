70e3cb650283d75b764eb97d3c1aa01361d877a6
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-02 09:32:50 +0100

    rotate_point() function defined

--- rotace_primky_kolem_bodu/rotace.py
+++ rotace_primky_kolem_bodu/rotace.py
@@ -9,11 +9,12 @@
 # IMPORTS #
 import math
 
+
 # CLASSES #
 class Abscisse:
     def __init__(self,
-                 point_1, # a touple with x coordinates
-                 point_2, # a touple with y coordinates
+                 point_1,  # a touple with x coordinates
+                 point_2,  # a touple with y coordinates
                  ):
         """ Init method """
         self.point_1 = point_1
@@ -21,16 +22,23 @@ class Abscisse:
 
     def get_length(self):
         """ Returns a length of an abscisse """
-        return math.sqrt((self.point_1[0] - self.point_2[0])**2 + (self.point_1[1] - self.point_2[1])**2)
+        return math.sqrt((self.point_1[0] - self.point_2[0]) ** 2 + (self.point_1[1] - self.point_2[1]) ** 2)
 
     def rotate(self,
-               angle,               # an angle of rotation in degrees
-               point_of_rotation,   # rotate around this point
+               angle,  # an angle of rotation in degrees
+               point_of_rotation,  # rotate around this point
                ):
         """ Rotate an abscisse around a point with given angle.
-            Returns 4 points after rotation."""
-        pass
+            Returns 2 points after rotation."""
 
+        def rotate_point(point, point_of_rotation, angle):
+            """ Rotates a point around an angle and point"""
+            pass
+
+        point_1_rotated = rotate_point(self.point_1, point_of_rotation, angle)
+        point_2_rotated = rotate_point(self.point_2, point_of_rotation, angle)
+
+        return point_1_rotated, point_2_rotated
 
 
 # RUNTIME #
@@ -46,10 +54,9 @@ def main():
     y_rot = float(input("Enter the Y coordinate of the point of rotation: "))
     angle_rot = float(input("Enter the angle in degrees: "))
 
-    abscisse = Abscisse((x1,y1), (x2,y2))
+    abscisse = Abscisse((x1, y1), (x2, y2))
 
     print(f"Length of an abscisse is: {abscisse.get_length()}")
-
 
 
 if __name__ == "__main__":
