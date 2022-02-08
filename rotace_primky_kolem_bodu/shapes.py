2340facfa8f0b41f6acf99de0eb9113b67acdc19
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-08 11:34:44 +0100

    Shapes program defined

---
+++ rotace_primky_kolem_bodu/shapes.py
@@ -0,0 +1,17 @@
+"""
+    SHAPES
+
+    A graphical program to deal with a shapes.
+
+    Using 'geometry' library.
+
+    Author:     Rudolf Klusal
+    Year:       2022
+"""
+
+# IMPORTS #
+import geometry
+
+# RUNTIME #
+if __name__ == "__main__":
+    """ Main function """
