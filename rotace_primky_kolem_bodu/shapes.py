f7dfb78b449fd333d19a7b8709cbe3d1efa415ee
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-08 11:49:29 +0100

    TkInter window completed, canvas displayed

--- rotace_primky_kolem_bodu/shapes.py
+++ rotace_primky_kolem_bodu/shapes.py
@@ -10,8 +10,23 @@
 """
 
 # IMPORTS #
+import tkinter
 import geometry
 
 # RUNTIME #
 if __name__ == "__main__":
     """ Main function """
+
+    # Creating a window
+
+    # Initialize TkInter
+    root_window = tkinter.Tk()
+
+    # Canvas
+    window_canvas = tkinter.Canvas(root_window, bg="white", width=800, height=600)
+
+    # Render canvas
+    window_canvas.pack()
+
+    # Wait for user input
+    root_window.mainloop()
