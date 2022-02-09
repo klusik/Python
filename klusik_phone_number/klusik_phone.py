baf197f866591543467e7a01f0882f4179cbc607
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-09 06:36:53 +0100

    It works (hopefuly)

--- klusik_phone_number/klusik_phone.py
+++ klusik_phone_number/klusik_phone.py
@@ -13,8 +13,67 @@ Examples:
 
 '''
 
+""" Author: klusik@klusik.cz """
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+
 def valid_phone_number(phone_number):
     """ Returns true if valid phone number """
 
+    # Length 14 chars
+    if not len(str(phone_number)) == 14:
+        logging.info("Length")
+        return False
+
+    # String
+    if not str(phone_number) == phone_number:
+        logging.info("Not a string")
+        return False
+
+    # Parentheses
+    if not (phone_number[0] == '(' and phone_number[4] == ')'):
+        logging.info("Parentheses")
+        return False
+
+    # Prefix number
+    if not str(phone_number[1:3]).isnumeric():
+        logging.info("Prefix")
+        return False
+
+    # A space
+    if not phone_number[5] == ' ':
+        logging.info("Space")
+        return False
+
+    # A triplet before dash
+    if not str(phone_number[6:8]).isnumeric():
+        logging.info("Triplet")
+        return False
+
+    # Dash
+    if not phone_number[9] == '-':
+        logging.info("Dash")
+        return False
+
+    # Final number
+    if not str(phone_number[10:]).isnumeric():
+        logging.info("Final number")
+        return False
+
+
+    # Everything seems ok
+    logging.info("OK")
+    return True
+
 if __name__ == "__main__":
+
+
     number = str(input("Enter the phone number: "))
+
+    if valid_phone_number(number):
+        print(f"Cool, {number} is a valid number.")
+    else:
+        print(f"Nah, {number} is not a valid number.")
