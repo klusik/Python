fedd60c987a2d5394a90bb7d8cb68ee9a5486084
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-09 10:53:00 +0100

    Basic methods done, let's test that

--- Bonusy/Morče code/morse.py
+++ Bonusy/Morče code/morse.py
@@ -8,7 +8,50 @@
 
 # CLASSES #
 class ConversionTable:
-    table = {'a':'.-'}
+    table = {
+        'a':'.-',
+        'b':'-...',
+        'c':'-.-.',
+        'd':'-..',
+        'e':'.',
+        'f':'..-.',
+        'g':'--.',
+        'h':'....',
+        'i':'..',
+        'j':'.---',
+        'k':'-.-',
+        'l':'.-..',
+        'm':'--',
+        'n':'-.',
+        'o':'---',
+        'p':'.--.',
+        'q':'--.-',
+        'r':'-.-',
+        's':'...',
+        't':'-',
+        'u':'..-',
+        'v':'...-',
+        'w':'.--',
+        'x':'-..-',
+        'y':'-.--',
+        'z':'--..',
+    }
+
+    @staticmethod
+    def get_morse(ascii):
+        """ Returns a morse code """
+        if ConversionTable.table[ascii]:
+            return ConversionTable.table[ascii]
+        else:
+            return 'X'
+
+    @staticmethod
+    def get_ascii(morse):
+        """ Returns ascii character """
+        if ConversionTable.table.keys()[morse]:
+            return ConversionTable.keys()[morse]
+        else:
+            return 'X'
 
 class Morse:
     """ Morse class """
@@ -19,9 +62,20 @@ class Morse:
 
     def encode(self):
         """ Encoding a message to Morse, returns a morse code """
+        output = list()
+        for character in self.message:
+            output.append(ConversionTable.get_morse(character))
+
+        return " ".join(output)
 
     def decode(self):
         """ Decoding a message in Morse, returns a string """
+        output = list()
+        encoded = str(self.message).split()
+        for morse in encoded:
+            output.append(ConversionTable.get_ascii(morse))
+
+        return "".join(output)
 
 
 
