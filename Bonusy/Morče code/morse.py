95f9d0703f4a400af81f251b992fdc4f8d885e1b
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-09 10:40:16 +0100

    Defining basic classes and methods

--- Bonusy/Morče code/morse.py
+++ Bonusy/Morče code/morse.py
@@ -1,5 +1,29 @@
 """
 
     A library containing encode and decode methods
-    
+
 """
+
+# IMPORTS #
+
+# CLASSES #
+class ConversionTable:
+    table = {'a':'.-'}
+
+class Morse:
+    """ Morse class """
+    def __init__(self,
+                 message, # A message to encode or decode
+                 ):
+        self.message = message
+
+    def encode(self):
+        """ Encoding a message to Morse, returns a morse code """
+
+    def decode(self):
+        """ Decoding a message in Morse, returns a string """
+
+
+
+
+# RUNTIME #
