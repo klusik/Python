a2e6b668cb8098378d39fc9cec065512a473a4e3
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-14 14:10:54 +0100

    Basic logic slowly emerges

--- Bonusy/7kyu_math/7kyu_math.py
+++ Bonusy/7kyu_math/7kyu_math.py
@@ -11,12 +11,55 @@
 """
 
 # RUNTIME #
-def do_computation(input_string):
+def parse_input(input_string):
     """ Returns a number, reads a string """
 
     # This function tries to read all numbers and 'plus' or 'minus'
     # substrings, ignores all other stuff.
     # It should be okay for 'pl us' and so on.
+
+    # Empty input
+    if len(input_string) == 0:
+        return False
+
+    # Not empty input
+    plus = False            # Plus trigger
+    minus = False           # Minus trigger
+    number = list()         # List of detected numbers (build up numbers)
+    running_sum = list()    # List of previously found numbers and operations
+
+    for index, character in enumerate(input_string):
+        if not (plus and minus):
+            # Basic branch -- no sign of sign detected yet
+
+            # Is a character character or a digit?
+            if str(character).isnumeric():
+                # It's a number
+                number.append(character)
+
+                # reset plus or minus
+                plus = False
+                minus = False
+
+            if str(character).isalpha():
+                # Begining of 'plus' or 'minus' (maybe)
+                if str(character).lower() == 'p':
+                    plus = True
+                    minus = False
+
+                if str(character).lower() == 'm':
+                    minus = True
+                    plus = False
+
+                # Handling previous numbers
+                if len(number) > 0:
+                    # Not empty list of digits
+                    running_sum.append(int("".join(number)))
+
+                    # Place for a new number
+                    number.clear()
+
+        # In the case of plus or minus
 
 def main():
     """ Main function """
@@ -24,7 +67,7 @@ def main():
     input_string = str(input("Enter the string: "))
 
     try:
-        print(f"{input_string} = {do_computation(input_string)}.")
+        print(f"{input_string} = {parse_input(input_string)}.")
     except Exception as exception:
         print("Error occured :-(")
         raise exception
