981dd73f8807eb31305da184d67411ea51fdcb45
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-15 10:10:39 +0100

    Initial commit

---
+++ Bonusy/romanNumerals/roman.py
@@ -0,0 +1,15 @@
+# Roman numerals converter
+# (both ways)
+#
+# Author:   Rudolf Klusal 2022
+
+# IMPORTS #
+
+# CLASSES #
+
+# RUNTIME #
+def main()
+    """ Main function """
+    
+if __name__ == "__main__":
+    main()
