49af0bf9670fb4574883163ad73cf932a93b815d
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-16 10:44:27 +0100

    Classes defined

--- Bonusy/prime_product/prime_product.py
+++ Bonusy/prime_product/prime_product.py
@@ -5,3 +5,35 @@
     Author:     Rudolf Klusal 2022
 """
 
+# IMPORTS #
+
+# CLASSES #
+class Config:
+    """
+        Configuration
+    """
+    cache_file_name = "primes_cache.kls"
+
+class PrimeCache:
+    """
+        Caches all found primes to file,
+        so next time it won't be necessary
+        to compute them again and again
+    """
+
+class Primes:
+    """
+        Found primes
+    """
+
+class Product:
+    """
+        Handles the primes factorization
+    """
+
+# RUNTIME #
+def main():
+    """ Main runtime function """
+
+if __name__ == "__main__":
+    main()
