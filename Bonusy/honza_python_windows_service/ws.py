21ea6da5c71e58b00ab2f96f32a9ec625ea3667b
Rudolf Klusal <klusik@klusik.cz>
2022-02-17 13:30:36 +0100

    other reqs

--- Bonusy/honza_python_windows_service/ws.py
+++ Bonusy/honza_python_windows_service/ws.py
@@ -2,4 +2,6 @@
 # Honza Hrádek & windows service
 #
 # Requirements:
-#   Local admin, pywin32 (probably)
+#   Local admin, pywin32 (module install)
+#   Write a code, write a wrapper.
+#   No GUI, no dialogues, use web service or something like that
