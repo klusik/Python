be02562bc34a0a67baf096fd21e28e21f29eee77
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-28 14:40:42 +0100

    TK: Playing with settings

--- Bonusy/tkinter_stuff/tkinter_stuff.py
+++ Bonusy/tkinter_stuff/tkinter_stuff.py
@@ -5,10 +5,23 @@
 # IMPORTS
 import tkinter as tk
 
+# CONFIG
+FRAME_BG = "#ABCDEF"
+W_DIMENSIONS = "600x600"
+
 # CLASSES
 class App:
     def __init__(self):
+        # Window
         self.window = tk.Tk()
+        self.window.geometry(W_DIMENSIONS)
+        self.window.title("Test TK")
+        
+        # Frame
+        self.frame = tk.Frame(self.window, bg=FRAME_BG)
+
+        self.frame.pack(expand=True, fill="both")
+        self.frame.update()
 
     def run(self):
         self.window.mainloop()
