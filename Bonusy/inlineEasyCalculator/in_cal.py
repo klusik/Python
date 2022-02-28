967c9d01ba03fead7c3a270400ae60ad7ef98aaa
Rudolf Klusal <rudolf.klusal@zf.com>
2022-02-28 06:41:13 +0100

    Inline calculator: Initial commit.

---
+++ Bonusy/inlineEasyCalculator/in_cal.py
@@ -0,0 +1,18 @@
+"""
+    Based on 'pyladies'
+
+    Basic inline calculator with +, -, * and /
+
+    () in the Future :-)
+
+    Author:     klusik 2022
+"""
+
+# IMPORTS #
+
+# RUNTIME #
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
