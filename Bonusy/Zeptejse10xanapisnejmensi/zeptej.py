5272ccd9d654616a0e76b30cfaeedf853b0077c7
Rudolf Klusal <rudolf.klusal@zf.com>
2022-03-03 14:00:38 +0100

    comment

--- Bonusy/Zeptejse10xanapisnejmensi/zeptej.py
+++ Bonusy/Zeptejse10xanapisnejmensi/zeptej.py
@@ -3,9 +3,11 @@ if __name__ == "__main__":
     for order in range(10):
         cislo = int(input("Napis cislo: "))
         if order:
+            # not first
             if cislo < minimum:
                 minimum = cislo
         else:
+            # first
             minimum = cislo
 
     print(f"Minimalni cislo je {minimum} :-)")
