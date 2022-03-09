28a84e0a76c8b045fb99aae8ded43defbc2ee455
Michal Sykora <michal.sykora@zf.com>
2022-03-09 10:35:18 +0100

    vcelka maja

--- michal/otters/vcelka_maja.py
+++ michal/otters/vcelka_maja.py
@@ -1,5 +1,23 @@
 # file for amusement of my litte child 
+# IMPORTS
+import turtle
 
+# define gif file as new coursor
+maja_path = r"C:\Users\Z0001932\OneDrive - ZF Friedrichshafen AG\prace\python\projects\streda\python_git\michal\PyLadies\03\maja.gif"
+
+# save defined gif file as a available shave in module turtle
+turtle.register_shape(maja_path)
+
+# set the maja shape active
+turtle.shape(maja_path)
+
+# non-funcitonal code
+# failed attempt to resize new coursor
+# turtle.resizemode("user")
+# turtle.shapesize(0.2, 0.2, 1)
+
+# test only Maja
+'''
 def Maja(n):
     for i in range(n):
         if i in [n/10, n/20, n/30, n/40, n/50, n/60, n/70, n/80, n/90]:
@@ -10,4 +28,14 @@ def Maja(n):
             print('KONCIM SVUJ LET')
         else: 
             print('>>> LETIM. BZZZZZZZZZ')
-Maja(30000)
+'''
+
+def Maja_g():
+    '''
+    Draws custom shaped coursor ...
+    '''
+    
+    pass
+
+# call of text only Maja()
+# Maja(30000)
