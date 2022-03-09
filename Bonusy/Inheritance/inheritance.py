c4d8fef071f64f2d88985653df0d9879316056a9
Rudolf Klusal <klusik@klusik.cz>
2022-03-09 23:48:46 +0100

    Initial commit

---
+++ Bonusy/Inheritance/inheritance.py
@@ -0,0 +1,23 @@
+"""
+    Explore OOP inheritance
+"""
+class Color:
+    pass
+
+class Liquid:
+    def __init__(self,
+                 mass_concentration
+                 ):
+        self.liquid = True
+        self.mass_concentration = mass_concentration
+        self.color=None
+
+class Milk(Liquid):
+    def __init__(self):
+        super().__init__(1)
+        self.color="white"
+
+if __name__=="__main__":
+    milk = Milk()
+
+    print(milk.color, milk.mass_concentration)
