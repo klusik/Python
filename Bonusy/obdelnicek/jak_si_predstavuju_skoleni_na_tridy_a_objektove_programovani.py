dee5e4494a23cd1085e7bd0483cee607493efb08
Rudolf Klusal <rudolf.klusal@zf.com>
2022-03-09 11:27:00 +0100

    Just pokus

---
+++ Bonusy/obdelnicek/jak_si_predstavuju_skoleni_na_tridy_a_objektove_programovani.py
@@ -0,0 +1,24 @@
+class Point:
+    def __init__(self, x, y):
+        print(f"Vytváří se objekt se souřadnicemi {x} a {y}.")
+        self.x = x
+        self.y = y
+
+
+
+point_x = 10
+point_y = 20
+
+list_of_points = [(10,20), (30,40)]
+
+print(f"Soudarrnice jsou {point_x} a {point_y}.")
+
+print(f"Souracnice všech bodů jsou {list_of_points}")
+
+# co by se mi líbilo
+bod_pocatek = Point(0, 0)
+
+bod_kam_jdu = Point(10, 20)
+
+print(bod_pocatek.x)
+
