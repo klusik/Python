23cea14138297a416dcd2bec163dd5ea173ed496
Rudolf Klusal <rudolf.klusal@zf.com>
2022-03-16 12:46:34 +0100

    Randomly puts circles & ... others (forgot the name, shit)

--- Bonusy/ASCII_pišqorky/pisqorky.py
+++ Bonusy/ASCII_pišqorky/pisqorky.py
@@ -20,11 +20,16 @@ Napiš funkci vyhodnot, která dostane řetězec s herním polem 1-D piškvorek 
 """
 # IMPORTS #
 import random
+import logging
 
 # CLASSES #
 class Config:
     # Default gameboard size
     default_game_size = 20
+
+    @staticmethod
+    def logging_on():
+        logging.basicConfig(level=logging.INFO)
 
 class Game:
     def __init__(self,
@@ -33,25 +38,51 @@ class Game:
         # Game size
         self.size = size
 
+        # Game board
+        self.game_board = [0 for _ in range(size)]
+        logging.info(self.game_board)
+
         # Player pool
         self.players = list()
 
     def finished(self):
         """ Checks if the game is finished or not.
             Returns True if it is, False if not. """
-        return False
+
+        # If there's no other place to put another cross or circle
+        if not 0 in self.game_board:
+            return True
 
     def add_player(self,
-                   name=None,  # set up a name if you want
+                   name=None,           # set up a name if you want
                    count_of_players=1,  # Append more players (batch adding)
+                   symbol=None,         # Default symbol for player
                    ):
         """ Adds players (default 1) """
         for _ in range(count_of_players):
             self.players.append(Player(name))
 
+        logging.info(self.players)
+
     def play(self):
         """ Player plays his/her turn """
-        turn = random.randrange(0, 20)
+
+        # Iterate through all players and place their
+        # symbols onto gaming board.
+
+        turn = 0
+        for player in self.players:
+            while True:
+                # Generate new turn until possible to play,
+                # you can't place a players field if already used.
+                turn = random.randrange(Config.default_game_size)
+                logging.info(f"Turn: {turn}")
+
+                if self.game_board[turn] == 0:
+                    break
+
+            # 'turn' has a coordinate of possible turn
+            self.game_board[turn] = player
 
 
 class Player:
@@ -64,6 +95,10 @@ class Player:
 def game():
     """ Main runtime """
 
+    # Config
+    # Enable logging
+    Config.logging_on()
+
     # Create a gameboard
     game_board = Game(size=20)
 
@@ -73,6 +108,7 @@ def game():
 
     while not game_board.finished():
         """ Do game :-) """
+        game_board.play()
 
 
 
