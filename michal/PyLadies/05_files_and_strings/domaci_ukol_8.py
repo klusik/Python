9c5089b73fcd26464758de4e0cfd3acf2cf60d58
kopfrkingl <sykora.michal1@gmail.com>
2022-03-20 15:26:56 +0100

    PyLadies - Lecture 5 - files and strings home work 8 in progress

---
+++ michal/PyLadies/05_files_and_strings/domaci_ukol_8.py
@@ -0,0 +1,38 @@
+'''
+Napiš funkci tah, která dostane řetězec s herním polem, číslo políčka (0-19),
+a symbol (x nebo o) a vrátí herní pole (t.j. řetězec) s daným symbolem 
+umístěným na danou pozici.
+
+Hlavička funkce by tedy měla vypadat nějak takhle:
+
+def tah(pole, cislo_policka, symbol):
+    "Vrátí herní pole s daným symbolem umístěným na danou pozici"
+    ...
+
+'''
+
+def turn(field, position, symbol):
+    '''
+    Return updated game field with given symbol placed into original game filed.
+    Positions are being calculated from 0
+
+    INPUT
+    field - field consisting of '-', 'x' or 'o', string
+    position - index number where new char should be placed, integer
+    symbol - players symbol 'x' or 'o', string
+
+    RETURN
+    new_field - new game field including players symbol
+    '''
+    # divide field to three parts, pre-position, position and after-position
+    pre_position_field = field[:position]
+
+    # post position 
+    post_position_field = field[position:]
+
+    # test for available position
+    if (field[position] == '-'):
+        print('invalid position')
+    else:
+        # concatenate three parts into output game field
+        new_field = pre_position_field + symbol + post_position_field
