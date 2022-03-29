8d852cf3ab466fb241fcbd68975ac2293684fc09
Rudolf Klusal <klusik@klusik.cz>
2022-03-29 21:55:04 +0200

    All three functions :-)

--- Bonusy/PyLadies/lists_lesson.py
+++ Bonusy/PyLadies/lists_lesson.py
@@ -26,6 +26,10 @@ def select_correct(list_of_names):
         correct_name = True # Assume a correct name
         splitted_name = name.split()
 
+        if not len(splitted_name) == 2:
+            correct_name = False
+            continue # next name
+
         for name_part in splitted_name:
             if name_part[0].islower():
                 correct_name = False
@@ -38,12 +42,48 @@ def select_correct(list_of_names):
 
 
 def select_incorrect(list_of_names):
-    pass
+    """ Returns a list of all incorrect names """
+    correct_names = select_correct(list_of_names)
+
+    incorrect_names = []
+    for name in list_of_names:
+        # Just using existing function :-D
+        if name not in correct_names:
+            incorrect_names.append(name)
+
+    return incorrect_names
+
 
 def repair_incorrect(list_of_names):
-    pass
+    """ Repair names """
+
+    incorrect_names = select_incorrect(list_of_names)
+
+    corrected_names = []
+
+    for name in list_of_names:
+
+        if name in incorrect_names:
+            if not len(name.split()) == 2:
+            # Correcting by deleting all middle names
+                name = f"{name.split()[0]} {name.split()[-1]}"
+
+            # Split it by name & surname
+            name_first, name_second = name.split()
+
+            corrected_names.append(f"{name_first.capitalize()} {name_second.capitalize()}")
+            continue # next name
+
+        # If not incorrect, add that name as is
+        corrected_names.append(name)
+
+    return corrected_names
+
 
 # RUNTIME #
 if __name__ == "__main__":
-    names = ['jan Novák', 'Michal Sýkora', 'Rudolf klusal', 'kuba červinka']
-    print(select_correct(names))
+    names = ['jan Novák', 'Michal Sýkora', 'Rudolf klusal', 'kuba červinka', 'Jakub Jan Ryba']
+    print(f"Original names: {names}")
+    print(f"Correct names: {select_correct(names)}")
+    print(f"Incorrect names: {select_incorrect(names)}")
+    print(f"Corrected names: {repair_incorrect(names)}")
