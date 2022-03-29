5cc0fbbe440acc3ff3c9d673f02e82232761fe5e
Rudolf Klusal <klusik@klusik.cz>
2022-03-29 19:12:32 +0200

    Initial comment

---
+++ Bonusy/PyLadies/lists_lesson.py
@@ -0,0 +1,14 @@
+"""
+Představ si, že ti uživatelé zadávají jména a příjmení a ty si je
+ukládáš do seznamu pro další použití např. v evidenci studentů.
+Ne všichni jsou ale pořádní, a tak se v seznamu sem tam objeví
+i jméno s nesprávně zadanými velkými písmeny. Například:
+
+zaznamy = ['pepa novák', 'Jiří Sládek', 'Ivo navrátil', 'jan Poledník']
+Úkolem je:
+
+Napsat funkci, která vybere jen ty správně zadané záznamy,
+které mají správně jméno i příjmení s velkým počátečním písmenem.
+Napsat funkci, která vybere naopak jen ty nesprávně zadané záznamy.
+(Nepovinný) – Napsat funkci, která vrátí seznam s opravenými záznamy.
+"""
