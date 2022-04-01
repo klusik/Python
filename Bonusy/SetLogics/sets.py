e98fc89a13b9ad4875c48309a9c832cd2a0aadeb
Rudolf Klusal <rudolf.klusal@zf.com>
2022-04-01 13:50:48 +0200

    AND & OR

--- Bonusy/SetLogics/sets.py
+++ Bonusy/SetLogics/sets.py
@@ -1,4 +1,25 @@
 """
 Testing a set logics
 """
+sets = list()
+sets.append(set())
+sets.append(set())
+
+sets[0].add("svíčková")
+sets[0].add("mléko")
+sets[0].add("kakao")
+sets[0].add("zmrzlina")
+sets[0].add("voda")
+
+sets[1].add("barák")
+sets[1].add("slon")
+sets[1].add("automobil")
+sets[1].add("zf")
+sets[1].add("voda")
+
+print(f"Všechno: {sets}")
+
+print(f"OR: {sets[0] | sets[1]}")
+
+print(f"AND: {sets[0] & sets[1]}")
 
