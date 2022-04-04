00602dbd74819460b610f6583358658946f338d2
Rudolf Klusal <rudolf.klusal@zf.com>
2022-04-04 06:45:07 +0200

    Initial commit

---
+++ Bonusy/FolderUnpacker/unpack.py
@@ -0,0 +1,14 @@
+"""
+    Unpack all folders, merge all the files inside,
+    rename the files via wildcards
+
+    Author: klusik@klusik.cz
+"""
+
+# IMPORTS #
+
+# CLASSES #
+
+# RUNTIME #
+if __name__ == "__main__":
+    pass
