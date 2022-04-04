c3f94a11ce028b48d92aaf9f096e9464747a3a3f
Rudolf Klusal <rudolf.klusal@zf.com>
2022-04-04 07:04:34 +0200

    Specify default behaviour

--- Bonusy/FolderUnpacker/unpack.py
+++ Bonusy/FolderUnpacker/unpack.py
@@ -3,6 +3,12 @@
     rename the files via wildcards
 
     Author: klusik@klusik.cz
+
+    Default settings:
+    -   Reads all file names in all subfolders
+    -   Copies all files into another folder (default name "_unpacked")
+    -   Renames files as "file_#####.<original extension>" where "#" number
+        is dependent on how many files are there.
 """
 
 # IMPORTS #
