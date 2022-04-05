41b01ee768bbc2dfe310b0eb421ff8b459c860dc
Rudolf Klusal <klusik@klusik.cz>
2022-04-05 18:23:42 +0200

    Basic testing for repeated roman characters

--- Bonusy/romanNumerals/roman.py
+++ Bonusy/romanNumerals/roman.py
@@ -71,8 +71,15 @@ class Number:
             if not_valid:
                 exit()
 
+
+
             # Valid roman numeral
             self.roman_number = str(input_number).lower()
+
+            if not self.valid_roman():
+                print(f"Not valid (too many repeats of single character) in {self.roman_number}.")
+                exit()
+
             self.is_roman = True
             self.is_integer = False
             print("Number recognized as roman.")
@@ -86,6 +93,33 @@ class Number:
         """ Checks if enterer roman value is valid.
             Returns True if valid, False if invalid
         """
+        # Goes through all characters in roman numeral
+        # and checks if there is more than 3 same letters
+        # consequently
+
+        # counters
+        counter = 0
+        last_character = str()
+
+        for char_index, roman_character in enumerate(self.roman_number):
+            if char_index == 0:
+                # first letter
+                last_character = roman_character
+                continue
+
+            # For not first letter assume there is 'last_character' previously created
+            if last_character == roman_character:
+                counter += 1
+            else:
+                counter = 0
+                last_character = roman_character
+
+            if counter >= 3:
+                # Too many characters
+                return False
+
+        return True
+
     def convert_to_roman(self):
         """
         Converts self.number to roman
