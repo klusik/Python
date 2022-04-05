38fcc503c7b96b05ca0be834ab3c52237c962827
Michal Sykora <michal.sykora@zf.com>
2022-04-05 13:48:38 +0200

    PyLadies - Lecture 7 - Lists home work 7 - roman numbers - finished

--- michal/PyLadies/07_lists/domaci_ukol_7.py
+++ michal/PyLadies/07_lists/domaci_ukol_7.py
@@ -14,30 +14,10 @@ def rom_to_int(whole_roman_number):
 
     OUTPUT
     result = converted number, integer
-
-    DETAILS
-    Implementation idea is to start from right (smallest number) and compare
-    next number. If next number is bigger, add the next_number to result, 
-    else subtract number from result.
-
-    EXAMPLE
-
-    MCMXCIV = 1994
-    i = V -> V = 5
-    i + 1 = I -> I = 1
-    (i + 1) < i => result = 5 - 1 
-
-    next iteration
-    i = I
-    i + 1 = C -> C = 100
-    i + 1 > i => result + C
-    result = 104
-
-    if i + 1 = None: return result
     '''
     
     # keeps track of value of roman number
-    result = 5
+    result = 0
     
     # definition table of roman numbers
     table = [
@@ -58,11 +38,26 @@ def rom_to_int(whole_roman_number):
     # minuses when looking for index of letter
     whole_roman_number = whole_roman_number[::-1]
 
-    for current_roman in whole_roman_number:
+    # flag skip, used for skipping next letter in 'iv' pattern and similar
+    skip = False
+    
+    # index used for easy recognition of next number
+    for index, current_roman in enumerate(whole_roman_number):
+        
+        # 'iv' pattern handle
+        if skip:
+            skip = False
+            continue
+        
         # index in roman  string
-        next_roman_index = whole_roman_number.index(current_roman) + 1 
-        # value derived from index
-        next_roman_string = whole_roman_number[next_roman_index] 
+        next_roman_index = index + 1 
+        
+        # no next character
+        if next_roman_index >= len(whole_roman_number):
+            next_roman_string = current_roman
+        else:
+            # value of next roman string derived from index
+            next_roman_string = whole_roman_number[next_roman_index]
 
         # retrieve values from table
         current_roman_value = table[table.index(current_roman)-1]
@@ -71,10 +66,11 @@ def rom_to_int(whole_roman_number):
         # next is bigger
         if next_roman_value >= current_roman_value:
             # add current roman number and next number
-            result = next_roman_value + result
+            result = current_roman_value + result
         # next is smaller
         else:
-            result = result - next_roman_value
+            result = result + (current_roman_value - next_roman_value)
+            skip = True
             
     return result
 
