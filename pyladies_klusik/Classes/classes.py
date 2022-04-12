3a72214127b2d77ab520a13153ffc7668753d19d
Rudolf Klusal <klusik@klusik.cz>
2022-04-12 19:40:45 +0200

    Fujkód

--- pyladies_klusik/Classes/classes.py
+++ pyladies_klusik/Classes/classes.py
@@ -1,6 +1,7 @@
 """ Napiš třídu čtverec """
 
 class Ctverec:
+    pocet_stran = 4
     def __init__(self,
                  delka_strany):
         self.delka_strany = delka_strany
@@ -13,6 +14,9 @@ class Ctverec:
 
 
 ctv = Ctverec(10)
+Ctverec.pocet_stran = 5
+
+print(ctv.pocet_stran)
 
 print(ctv.vypocti_obvod())
 print(ctv.vypocti_obsah())
