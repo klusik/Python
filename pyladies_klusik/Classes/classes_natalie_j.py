19e1183165af7343a158ef403f1bfa089ae50686
Rudolf Klusal <rudolf.klusal@zf.com>
2022-04-19 18:26:51 +0200

    Natalie

--- pyladies_klusik/Classes/classes_natalie_j.py
+++ pyladies_klusik/Classes/classes_natalie_j.py
@@ -1,19 +1,25 @@
 class Ctverec():
     def __init__(self, strana):
         self.strana = strana
+
     def obvod(self):
-        print(f"{self.strana*4 }")
+        return self.strana * 4
+
     def obsah(self):
-        print(f"{self.strana**2 }")
+        return self.strana ** 2
+
     def rozdil_obsahu(self, jiny_ctverec):
-        self.jiny_ctverec = jiny_ctverec
-        print(f"{self.strana**2 - self.jiny_ctverec}")
-ctverecek = Ctverec(8)
+        return self.obsah() - jiny_ctverec.obsah()
+
+
+ctverecek = Ctverec(4)
 ctverecek.obvod()
 ctverecek.obsah()
-ctverecek.rozdil_obsahu(36)
 
-ctverecek2 = Ctverec(10)
+ctverecek2 = Ctverec(2)
+
+rozdil = ctverecek.rozdil_obsahu(ctverecek2)
+
+ctverecek2 = Ctverec(6)
 ctverecek2.obsah()
-
-ctverecek2.rozdil_obsahu(ctverecek)
+ctverecek2.rozdil_obsahu(8)
