491964f0ad31095aa0664b4bb51bca0953d0f93c
Rudolf Klusal <klusik@klusik.cz>
2022-04-20 22:23:29 +0200

    Initial commit

---
+++ Bonusy/Reversed Order (C)/Reversed order/reversed.c
@@ -0,0 +1,17 @@
+/*
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+0 => [0]
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
