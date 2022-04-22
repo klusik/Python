77ef4473f29ba42ae8afc4a43b224d6f9c4abd1e
Rudolf Klusal <rudolf.klusal@zf.com>
2022-04-22 11:04:45 +0200

    Basic simulation

--- Bonusy/random_population_growth/Random_population_growth.py
+++ Bonusy/random_population_growth/Random_population_growth.py
@@ -15,6 +15,7 @@
 # IMPORTS #
 import random
 
+
 # CLASSES #
 class Defaults:
     """ Default constants and stuff """
@@ -50,18 +51,42 @@ class Person:
         pass
 
     def __repr__(self):
-        pass
+        output_string = ""
+        return (output_string)
+
 
 class Simulation:
     def __init__(self):
-        pass
+        # Set up defaults
+        self.iteration = 1
+
+        # Create a population
+        self.population = []
+
+        for _ in range(Defaults.initial_population):
+            self.population.append(Person())
 
     def __repr__(self):
-        pass
+        output_string = "-" * 30 + "\n"
+        output_string += f"Iteration:\t\t{self.iteration}\n"
+        output_string += f"Population: \t{self.count_persons()}"
+        return (output_string)
+
+    def count_persons(self):
+        return len(self.population)
+
+    def step(self):
+        self.iteration += 1
+
 
 # RUNTIME #
 def main():
-    pass
+    simulation = Simulation()
+
+    for step in range(Defaults.maximal_number_of_iterations):
+        print(simulation)
+        simulation.step()
+
 
 if __name__ == "__main__":
     main()
