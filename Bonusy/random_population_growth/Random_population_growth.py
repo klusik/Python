72ff00dd1b120e9409ace58fef04c4fb34ba63b8
Rudolf Klusal <rudolf.klusal@zf.com>
2022-04-22 10:45:00 +0200

    First requirements

---
+++ Bonusy/random_population_growth/Random_population_growth.py
@@ -0,0 +1,18 @@
+"""
+    The goal is to simulate a unbounded population growth
+    with added randomness.
+
+    There will be some constants (user input):
+
+        -- probability of birth of every two M/F for another person
+        -- age of concieving (all the same)
+        -- max age of concieving (all the same)
+        -- max age (all the same), after this age random death occurs
+
+"""
+
+# IMPORTS #
+
+# CLASSES #
+
+# RUNTIME #
