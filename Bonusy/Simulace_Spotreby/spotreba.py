3a7ae8ec91efc3010a8128ee196b8118e91ce1c8
Rudolf Klusal <klusik@klusik.cz>
2022-04-24 21:45:24 +0200

    Initial commit -- what to do next, Michal? :-D

---
+++ Bonusy/Simulace_Spotreby/spotreba.py
@@ -0,0 +1,26 @@
+"""
+    How to simulate a car (it's consumption)
+
+    I don't know what to do more :-D
+"""
+
+# CLASSES #
+class Car:
+    def __init__(self,
+                 valves = 4,
+                 ventiles = 16,
+                 ):
+        self.valves = valves
+        self.ventiles = ventiles
+
+    def magic(self) -> bool:
+        return True
+
+
+# RUNTIME #
+def main():
+    car = Car()
+
+
+if __name__ == "__main__":
+    main()
