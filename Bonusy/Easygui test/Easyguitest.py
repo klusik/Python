ef922278622225f1c9b9d022ce0d6c7550d45312
Rudolf Klusal <klusik@klusik.cz>
2022-04-27 00:16:43 +0200

    The best logic ever :-D

--- Bonusy/Easygui test/Easyguitest.py
+++ Bonusy/Easygui test/Easyguitest.py
@@ -8,4 +8,10 @@ import easygui
 # RUNTIME #
 if __name__ == "__main__":
     """ Main magic :-) """
-    
+
+    answer = easygui.ynbox("Is this the best app ever?", "The best dialogue box ever", ("Yes", "Also Yes"))
+
+    if answer:
+        easygui.msgbox(f"Wise words.")
+    else:
+        easygui.msgbox("Also wise words.")
