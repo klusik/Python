eb4f210902f6527291f05f974895bd1554b174d5
Rudolf Klusal <rudolf.klusal@zf.com>
2022-04-28 13:48:01 +0200

    Main structure, initial commit

---
+++ Bonusy/Body values/body_values.py
@@ -0,0 +1,29 @@
+"""
+    Body values
+
+    For losing weight purposes
+"""
+
+# IMPORTS #
+import easygui
+import xml
+
+# CLASSES #
+class Body:
+    def __init__(self):
+        pass
+
+
+class BodyValues:
+    def __init__(self):
+        pass
+    
+
+
+# RUNTIME #
+def main():
+    # Create a body
+    body = Body()
+
+if __name__ == "__main__":
+    main()
