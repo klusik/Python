f5516b26c38f3e2011aba83264f4aa4f6f04f645
Rudolf Klusal <rudolf.klusal@zf.com>
2022-05-05 14:18:10 +0200

    PEP8

--- Bonusy/Body values/body_values.py
+++ Bonusy/Body values/body_values.py
@@ -14,10 +14,11 @@ import xml.etree.ElementTree as xmlElTree
 # CLASSES #
 class Helper:
     """ Stuff that helps """
+
     @staticmethod
-    def dict_dump(variable, # variable which is readed
-                  indent=0, # indent of the print
-                  indent_symbol = ' ', # symbol of the indent, default ' '
+    def dict_dump(variable,  # variable which is readed
+                  indent=0,  # indent of the print
+                  indent_symbol=' ',  # symbol of the indent, default ' '
                   ):
         """ Dumps variable in readable way """
 
@@ -25,11 +26,11 @@ class Helper:
         # are values or dicts.
 
         for index, item in enumerate(variable.items()):
-            print(f"{str(indent_symbol)*indent}{item[0]}")
+            print(f"{str(indent_symbol) * indent}{item[0]}")
             if isinstance(item[1], dict):
-                Helper.dict_dump(item[1], indent+1, ' ')
+                Helper.dict_dump(item[1], indent + 1, ' ')
             else:
-                print(f"{str(indent_symbol)*(indent+1)}{item[1]}")
+                print(f"{str(indent_symbol) * (indent + 1)}{item[1]}")
 
 
 class Config:
