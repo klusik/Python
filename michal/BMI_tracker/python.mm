9778bcb9472391fb0dbebfc5efd3abf38443337e
Michal Sýkora <michal.sykora@zf.com>
2022-05-06 15:11:38 +0200

    Michal - BMI tracker  - task description in mindmap  - added empty files:                      calculations.py                      gui.py                      main.py                      python                      user_input.py

---
+++ michal/BMI_tracker/python.mm
@@ -0,0 +1,125 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="python" FOLDED="false" ID="ID_1800937960" CREATED="1651841377871" MODIFIED="1651841403009" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" background="#3c3f41">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" associatedTemplateLocation="template:/Darcula-1.7.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_908385986" ICON_SIZE="12 pt" COLOR="#cccccc" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_908385986" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3300">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#bbbbbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<cloud COLOR="#666666" SHAPE="ARC"/>
+<node TEXT="knowledge base" FOLDED="true" POSITION="right" ID="ID_105242060" CREATED="1651841407925" MODIFIED="1651841415216">
+<edge COLOR="#dddd00"/>
+<node TEXT="pyladies" ID="ID_1202801828" CREATED="1651841715200" MODIFIED="1651841717825"/>
+</node>
+<node TEXT="projects" POSITION="right" ID="ID_227734937" CREATED="1651841416220" MODIFIED="1651841422408">
+<edge COLOR="#dd0000"/>
+<node TEXT="private" ID="ID_1633308677" CREATED="1651841427990" MODIFIED="1651841430880">
+<node TEXT="BMI_Tracker" ID="ID_1101099667" CREATED="1651841437517" MODIFIED="1651841445056">
+<node TEXT="specifications" ID="ID_983344223" CREATED="1651841452173" MODIFIED="1651841456681">
+<node TEXT="input" ID="ID_1725269782" CREATED="1651841530901" MODIFIED="1651841557457">
+<node TEXT="weight" ID="ID_158202596" CREATED="1651841485654" MODIFIED="1651841487936"/>
+<node TEXT="height" ID="ID_1154957954" CREATED="1651841488533" MODIFIED="1651841490385"/>
+<node TEXT="chest volume" ID="ID_1493459458" CREATED="1651841492069" MODIFIED="1651841503880"/>
+<node TEXT="belly volume" ID="ID_1787209703" CREATED="1651841504366" MODIFIED="1651841512025"/>
+<node TEXT="bottom volume" ID="ID_683576864" CREATED="1651841512278" MODIFIED="1651841516962"/>
+</node>
+<node TEXT="output" ID="ID_519571381" CREATED="1651841558078" MODIFIED="1651841560816">
+<node TEXT="locally saved xml file with input values" ID="ID_1135783464" CREATED="1651841594663" MODIFIED="1651841634363"/>
+<node TEXT="BMI" ID="ID_1657941758" CREATED="1651841649895" MODIFIED="1651841654305">
+<node TEXT="calculated" ID="ID_513133803" CREATED="1651841659263" MODIFIED="1651841663666"/>
+</node>
+<node TEXT="graph" ID="ID_559933548" CREATED="1651841732414" MODIFIED="1651841752106">
+<node TEXT="shown in GUI" ID="ID_1293423915" CREATED="1651841753047" MODIFIED="1651841756946"/>
+<node TEXT="exportable as PDF" ID="ID_1490769516" CREATED="1651841757655" MODIFIED="1651841765090"/>
+</node>
+<node TEXT="table" ID="ID_1446227679" CREATED="1651841767047" MODIFIED="1651841770270">
+<node TEXT="shown in GUI" ID="ID_982161552" CREATED="1651841770275" MODIFIED="1651841773138"/>
+<node TEXT="part of PDF export" ID="ID_291844264" CREATED="1651841774135" MODIFIED="1651841780995"/>
+</node>
+</node>
+<node TEXT="GUI" ID="ID_590183863" CREATED="1651841561998" MODIFIED="1651841565477">
+<node TEXT="tkinter wrapper" ID="ID_1285648785" CREATED="1651841906161" MODIFIED="1651841929538">
+<node TEXT="easygui" ID="ID_560614993" CREATED="1651842039146" MODIFIED="1651842043429"/>
+</node>
+</node>
+<node TEXT="architecture" ID="ID_299151586" CREATED="1651842088497" MODIFIED="1651842092874">
+<node TEXT="backend" ID="ID_1152676149" CREATED="1651842092879" MODIFIED="1651842101236">
+<node TEXT="all calculation in sperarate file calculations.py" ID="ID_1872098860" CREATED="1651842141073" MODIFIED="1651842169045"/>
+<node TEXT="user inputs handled in separate file user_input.py" ID="ID_1044769608" CREATED="1651842271096" MODIFIED="1651842285702"/>
+<node TEXT="main in separate file main.exe" ID="ID_643047511" CREATED="1651842169416" MODIFIED="1651842200213"/>
+</node>
+<node TEXT="frontend" ID="ID_858697824" CREATED="1651842102041" MODIFIED="1651842105181">
+<node TEXT="separated file GUI.py" ID="ID_139971489" CREATED="1651842107225" MODIFIED="1651842126510"/>
+<node TEXT="handling only windows" ID="ID_1144723053" CREATED="1651842127417" MODIFIED="1651842134479"/>
+</node>
+</node>
+</node>
+<node TEXT="implementation" ID="ID_1159384649" CREATED="1651841540022" MODIFIED="1651841545298"/>
+</node>
+</node>
+<node TEXT="professional" ID="ID_509289215" CREATED="1651841431781" MODIFIED="1651841434551"/>
+</node>
+</node>
+</map>
