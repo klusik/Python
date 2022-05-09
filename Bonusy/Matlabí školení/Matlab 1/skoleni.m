050a080c209d316bac904977d97a3c0127dcdbe5
Rudolf Klusal <rudolf.klusal@zf.com>
2022-05-09 10:38:56 +0200

    Vector stuff

--- Bonusy/Matlabí školení/Matlab 1/skoleni.m
+++ Bonusy/Matlabí školení/Matlab 1/skoleni.m
@@ -11,3 +11,48 @@
 
 % Matlab doplní do vektoru nuly, pokud daná pozice neexistuje (automaticky
 % prostě roztáhne pole)
+
+% Transpozice vektoru přes apostrof
+
+
+
+step = 0.01;
+x = 1:step:2;
+y = sin(2*pi*x);
+
+%plot(x,y);
+
+
+
+% Násobení po prvcích -- tečka, např. x.*y 
+
+
+x = 1:100;
+y = log(x);
+
+x.*y;
+
+
+% Výběr z řádku a sloupce:
+
+Matice = [1 2 3; 4 5 6; 7 8 9];
+
+Matice([1 2], [2 3])
+
+size(Matice)
+
+% Násobení matic atd.
+mg = magic(10);
+mg10 = mg*10;
+
+% Řešení soustavy lin. rovnic
+% y = x + 1
+% y = -2x + 4
+
+
+Matice_soustava = [-1 1; 2 1];
+vektor = [1 4]';
+
+inv(Matice_soustava) * vektor
+
+
