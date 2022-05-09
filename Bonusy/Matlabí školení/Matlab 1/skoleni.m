5c2a919ab05f35648f7dff1c39701e97d13dc9f0
Rudolf Klusal <rudolf.klusal@zf.com>
2022-05-09 09:29:23 +0200

    History

--- Bonusy/Matlabí školení/Matlab 1/skoleni.m
+++ Bonusy/Matlabí školení/Matlab 1/skoleni.m
@@ -1,3 +1,8 @@
 %% PROMENNE %%
 
-% bez diakritiky (ofc), nesmí začínat číslicí, je case sensitive %
+% bez diakritiky (ofc), nesmí začínat číslicí, je case sensitive 
+
+%% HISTORIE %%
+
+% Je možno použít CTRL pro výběr více příkazů z historie a vyvolat je
+% najednou (najs!)
