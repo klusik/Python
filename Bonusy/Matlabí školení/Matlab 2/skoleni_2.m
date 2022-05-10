a6df9e895c4e4780dd7d03996de98fe0b0149675
Rudolf Klusal <rudolf.klusal@zf.com>
2022-05-10 09:37:04 +0200

    Struktury

--- Bonusy/Matlabí školení/Matlab 2/skoleni_2.m
+++ Bonusy/Matlabí školení/Matlab 2/skoleni_2.m
@@ -1,5 +1,22 @@
 %% Školení MATLAB 2
 
+
+clc
+
+
 %% Další datové typy
 
 % cell {}
+
+cell_array = {[1, 2, 3], 'ahoj', "ahoj"};
+
+
+cell_array(1);
+cell_array{1};
+
+array_of_arrays = {["něco"], cell_array};
+
+array_of_arrays
+
+% struktury
+
