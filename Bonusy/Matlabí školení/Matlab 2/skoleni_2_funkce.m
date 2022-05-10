3a4475d5a5fead8282bb9af898276151f4672742
Rudolf Klusal <rudolf.klusal@zf.com>
2022-05-10 12:12:49 +0200

    nargout

--- Bonusy/Matlabí školení/Matlab 2/skoleni_2_funkce.m
+++ Bonusy/Matlabí školení/Matlab 2/skoleni_2_funkce.m
@@ -5,5 +5,9 @@ disp(the_best_function(1, 2, 3))
 
 the_best_function(1, 2, 3);
 
-% Zobrazit počet vstupů
+% Zobrazit (vrátit) počet vstupů
+% Možné použít na test, jestli je funkce správně
+% parametrizovaná.
+
+% Pro výstup je nargout
 nargin('the_best_function')
