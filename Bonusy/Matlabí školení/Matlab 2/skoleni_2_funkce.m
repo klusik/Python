70c9aab117f132c32baa1a3e1ae7cbd363b311e3
Rudolf Klusal <rudolf.klusal@zf.com>
2022-05-10 11:17:57 +0200

    Funkce (to be continued...)

--- Bonusy/Matlabí školení/Matlab 2/skoleni_2_funkce.m
+++ Bonusy/Matlabí školení/Matlab 2/skoleni_2_funkce.m
@@ -1,1 +1,6 @@
 %% Funkce
+clc
+
+disp(the_best_function(1, 2, 3))
+
+the_best_function(1, 2, 3);
---
+++ Bonusy/Matlabí školení/Matlab 2/the_best_function.m
@@ -0,0 +1,5 @@
+function return_value = the_best_function(a, b, c)
+    % This is a doc string
+    % We'll see what'll happen during the run
+
+    return_value = 6;
