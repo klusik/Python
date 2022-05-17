ba555de6b3b012253a2f67349cd0992d9539f03c
michal sykora <sykora.michal1@gmail.com>
2022-05-17 23:29:47 +0200

    label maker
    user input finished

--- michal/label_maker/user_input.py
+++ michal/label_maker/user_input.py
@@ -14,37 +14,47 @@ Example of output:
     }
 ]
 '''
-def user_input():
+def gather_data():
     '''Take input from user'''
     name = input('Zadejte jmeno: ')
     form = input('Zadejte formu: ')
     unit = input('Zadejte jednotky: ')
-    qty = input('zadejte pocet: ')
-    total_price = input('zadejte celkovou cenu: ')
+    qty = int(input('zadejte pocet: '))
+    total_price = float(input('zadejte celkovou cenu: '))
 
     return name, form, unit, qty, total_price
 
-def create_d():
-    name, form, unit, qty, total_price = user_input()
-    mydict = {
-        'name'          : name,
-        'form'          : form,
-        'units'         : unit,
-        'qty'           : qty,
-        'total_price'   : total_price 
-    }
+def user_input():
+    '''Call gather_data and save data in form of list of dicts.
+    
+    INPUT
+    gather_data()
 
+    OUTPUT
+    list_of_dicts
+    '''
     list_of_dicts = []
 
     while True:
-        cont = input('prejete si zadavat? a/n')
+        cont = input('prejete si zadavat? a/n: ')
 
-        if cont == 'a':
+        if (cont == 'a') or (cont == 'A'):
+            
+            name, form, unit, qty, total_price = gather_data()
+            mydict = {
+                'name'          : name,
+                'form'          : form,
+                'units'         : unit,
+                'qty'           : qty,
+                'total_price'   : total_price 
+            }
             list_of_dicts.append(mydict)
-        elif cont == 'n':
+            continue
+        elif (cont == 'n') or (cont == 'N'):
             break
         else:
             print('Neplatne zadani, konec')
-            break
+            continue
+    return list_of_dicts
 
-create_d()
+print(user_input())
