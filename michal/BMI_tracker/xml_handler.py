00181cde95bc1e4ae992043f689dd1a9242445af
michal sykora <sykora.michal1@gmail.com>
2022-05-18 15:04:01 +0200

    bmi tracker
    added jejich.xml as a reference file with correct syntax
    xml_handler.py - learning how to handle xml file

--- michal/BMI_tracker/xml_handler.py
+++ michal/BMI_tracker/xml_handler.py
@@ -1,11 +1,30 @@
 import xml.etree.ElementTree as ET
 
-def parseXML(file):
-    tree = ET.parse(file)
-    root = tree.getroot()
-    weight = root.findall(weight)
-    height = root.findall(height)
+def testujPraciSxml(file):
+    
+    tree = ET.parse(file) # tohle to najde
+    root = tree.getroot() # tohle to najde
+    
+    # weight = root.findall('weight')
+    # height = root.findall('height')
 
-    print(weight,height)
+    for userID in root:
+        # projde pouze prvni uroven child
+        # takze vrati pouze user id
+        #print(userID.tag, userID.attrib)
+        pass
+    for entryID in userID:
+        #print(entryID.tag, entryID.attrib)
+        pass
 
-parseXML(r'C:\UserData\git_ws\myOnlyRealPrivateRepo\michal\BMI_tracker\testXML.xml')
+    userID = 0
+    entryID = 0
+    weight = 0
+    height = 0
+    chest = 0
+    belly = 0
+    bottom = 0
+
+    # takhle prochazim strom bez cyklu
+    print(root[userID][entryID][weight].tag)
+testujPraciSxml(r'C:\UserData\git_ws\myOnlyRealPrivateRepo\michal\BMI_tracker\testXML.xml')
