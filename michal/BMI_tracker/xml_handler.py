5c224bec23e4e7cd0e4d5bc35eb520dfa532c6fb
michal sykora <sykora.michal1@gmail.com>
2022-05-19 09:53:43 +0200

    bmi tracker
    xml_hanler.py - learned how to open file, parse file, save file

--- michal/BMI_tracker/xml_handler.py
+++ michal/BMI_tracker/xml_handler.py
@@ -1,21 +1,10 @@
 import xml.etree.ElementTree as ET
 
 def testujPraciSxml(file):
-    
+    # vytvor objekt xml souboru
     tree = ET.parse(file) # tohle to najde
+    # vytvor objekt s obsahem souboru (strukura)
     root = tree.getroot() # tohle to najde
-    
-    # weight = root.findall('weight')
-    # height = root.findall('height')
-
-    for userID in root:
-        # projde pouze prvni uroven child
-        # takze vrati pouze user id
-        #print(userID.tag, userID.attrib)
-        pass
-    for entryID in userID:
-        #print(entryID.tag, entryID.attrib)
-        pass
 
     userID = 0
     entryID = 0
@@ -27,4 +16,22 @@ def testujPraciSxml(file):
 
     # takhle prochazim strom bez cyklu
     print(root[userID][entryID][weight].tag)
+    print(root[userID][entryID][weight].text)
+
+    # nacti hodnoty tagu
+    for userID in root:
+        # projde pouze prvni uroven child
+        # takze vrati pouze user id
+        print(userID.tag, userID.attrib)
+        
+    for entryID in userID:
+        print(entryID.tag, entryID.attrib)
+        
+    # zapis dat do xml souboru
+    print(root[0][0][0].text)
+    root[0][0][0].text = str(90) # i kdyz je to cislo, musim zapsat string
+    print(root[0][0][0].text)
+
+    tree.write(r'C:\UserData\git_ws\myOnlyRealPrivateRepo\michal\BMI_tracker\testXML.xml')
+
 testujPraciSxml(r'C:\UserData\git_ws\myOnlyRealPrivateRepo\michal\BMI_tracker\testXML.xml')
