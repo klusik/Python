16a45a0ded8f9312182ee36336f36b97a7ad7700
Rudolf Klusal <klusik@klusik.cz>
2022-05-23 10:23:57 +0200

    Working version

--- Bonusy/codewars/Unique_of_order/unique.py
+++ Bonusy/codewars/Unique_of_order/unique.py
@@ -10,3 +10,30 @@ unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 unique_in_order([1,2,2,3,3])       == [1,2,3]
 """
+
+def unique_in_order(input_list):
+    """ Main function """
+
+    # Define output list
+    output = list()
+
+    # Go through all the items and add them to list
+    for item_from_list in input_list:
+        # If not first
+        if len(output):
+            # If not same as previous
+            if item_from_list != output[-1]:
+                output.append(item_from_list)
+        else:
+            # First
+            output.append(item_from_list)
+
+    return output
+
+
+if __name__ == "__main__":
+    input_value = "TEsssT"
+    input_list = [1, 2, 3, 'test', input_value]
+
+    print(unique_in_order(input_value))
+    print(unique_in_order(input_list))
