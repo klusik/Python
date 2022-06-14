87b6f17c34a0c59a1cfe3eadc1888f836e4e1e3d
michal sykora <sykora.michal1@gmail.com>
2022-06-14 19:56:08 +0200

    label maker
    added ouptup dict format
    added home work

--- michal/label_maker/calculation.py
+++ michal/label_maker/calculation.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+
 def calculate_unit_price(data):
     ''''
     Add key 'unit_price' key to dict, value = total_price / quantity
@@ -7,3 +10,47 @@ def calculate_unit_price(data):
         item['unit_price'] = item['total_price'] / item['qty']
 
     return data
+
+def prepare_output_dic(input_dict):
+    '''
+    Link data dict to ouptut dict as follows
+
+    input_dict              output_dict
+    form: str               top_row: str  
+    name: str               top_row: str
+    quantity: int           top_row: str
+    total_price: float      price: str -> middle row
+    unit: str               bottom_row: str
+    unit_price: float       bottom_row: str
+    '''
+
+    output_dict = {}
+
+    # alien code
+    #total_price = input_dict['total_price']
+    #price = f'{total_price},-' # middle row
+    #output_dict['price'] = price
+
+    top_row = f'{input_dict["name"]} {input_dict["form"]} {input_dict["qty"]} {input_dict["units"]}'
+    bottom_row = f'1{input_dict["units"]} = {input_dict["unit_price"]}CZK'
+    middle_row = f'{input_dict["total_price"]}'
+    output_dict['top_row'] = top_row
+    output_dict['middle_row'] = middle_row
+    output_dict['bottom_row'] = bottom_row
+
+    return output_dict
+
+def prepare_output_list(input_data):
+    output_list = []
+
+    for data in input_data:
+        item = prepare_output_dic(data)
+        output_list.append(item)
+
+    return output_list
+
+
+# home work: zajistete, aby vysledny list mel delku 36 polozek
+# kdyz bude list delsi, rozdel to
+# kdyz kratsi, dopln ''
+# pripravit si CSV file delsi nez 36 polozek
--- michal/label_maker/label_maker.py
+++ michal/label_maker/label_maker.py
@@ -20,7 +20,8 @@ def main():
     data = inputs.csv_input_hardwork()
     print(data)
     calculated_data = calculation.calculate_unit_price(data)
-    # print(calculated_data)
+    print(calculation.prepare_output_dic(calculated_data[0]))
+    print('output list: ', calculation.prepare_output_list(calculated_data))
     #outputs.to_console(calculated_data)
 
 if __name__ == '__main__':
