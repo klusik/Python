95e93ca420cfaef4865b0f48f3daea01f5bf4492
Rudolf Klusal <rudolf.klusal@zf.com>
2022-06-23 13:15:44 +0200

    Define task

---
+++ Bonusy/randomWalk/randomWalk.py
@@ -0,0 +1,11 @@
+"""
+    Do a random walk generator.
+
+    Inputs:
+        -   A distance from origin to travel to
+        -   Number of dimensions (2 or more) to consider
+
+    Output:
+        -   List of coordinates
+        -   File with list of coordinates
+"""
