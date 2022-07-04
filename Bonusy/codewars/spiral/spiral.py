ce4ee3b5def22cde3cbf55ac298119d21a0d0d87
Rudolf Klusal <rudolf.klusal@zf.com>
2022-07-04 09:22:30 +0200

    Describe an algorithm (what I want to do):
    # Test if it's possible to go in direction way
    #
    # If the location on the direction's way is out of the grid, change direction
    # If the location + 2 on the direction's way is used (1), change direction.
    #
    # Stop cycle if there's nowhere to go 'legally'

--- Bonusy/codewars/spiral/spiral.py
+++ Bonusy/codewars/spiral/spiral.py
@@ -14,6 +14,20 @@ def spiralize(size):
     for row in range(size):
         grid.append([0]*size)
 
+    ## Going through the grid & filling it up :-)
+
+    # Initial location
+    location = [0, 0]   # 0, 0 is the top left corner
+    direction = [1, 0]  # x diff, y diff
+
+    while True:
+        # Test if it's possible to go in direction way
+        #
+        # If the location on the direction's way is out of the grid, change direction
+        # If the location + 2 on the direction's way is used (1), change direction.
+        #
+        # Stop cycle if there's nowhere to go 'legally'
+
     return grid
 
 if __name__ == "__main__":
