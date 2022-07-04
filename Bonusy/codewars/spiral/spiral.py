84d7feeb4ba35639f0a7a04d88c88821d6012df0
Rudolf Klusal <rudolf.klusal@zf.com>
2022-07-04 09:05:25 +0200

    Grid init

--- Bonusy/codewars/spiral/spiral.py
+++ Bonusy/codewars/spiral/spiral.py
@@ -4,8 +4,19 @@
 """
 
 def spiralize(size):
-    pass
+    """The goal is to create a grid with a spiral,
+    begining with location (0 0)."""
+
+    # Making a grid of size {size}
+    grid = []
+
+    # Initialize grid
+    for row in range(size):
+        grid.append([0]*size)
+
+    return grid
 
 if __name__ == "__main__":
     size = 10
-    spiralize(size)
+    for row in spiralize(size):
+        print(row)
