67f4e21298c713eece92e8830fbe417a042df693
Rudolf Klusal <rudolf.klusal@zf.com>
2022-07-04 08:31:34 +0200

    Initial commit

---
+++ Bonusy/codewars/spiral/spiral.py
@@ -0,0 +1,11 @@
+"""
+    CW:         https://www.codewars.com/kata/534e01fbbb17187c7e0000c6/train/python
+    Author:     klusik@klusik.cz
+"""
+
+def spiralize(size):
+    pass
+
+if __name__ == "__main__":
+    size = 10
+    spiralize(size)
