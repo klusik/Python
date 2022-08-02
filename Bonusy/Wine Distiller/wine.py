762d121b60f28211a4dc90735c15c81386658f5e
Rudolf Klusal <rudolf.klusal@zf.com>
2022-08-02 07:25:34 +0200

    Constants

---
+++ Bonusy/Wine Distiller/wine.py
@@ -0,0 +1,14 @@
+"""
+    How wine & water dilutes together
+"""
+
+# CONSTANTS #
+initial_wine_volume = 1000 # in ml
+initial_water_volume = 1000 # in ml
+spoon_size = 10 # in ml
+no_of_tries = 1 # 1 means 1 transfer (using spoon twice)
+
+
+if __name__ == "__main__":
+    """ Do stuff """
+    
