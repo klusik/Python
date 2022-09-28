b0329424e25c69ceb0715b22026db3f571b6f92a
Rudolf Klusal <klusik@klusik.cz>
2022-09-28 04:19:14 +0200

    User input

--- Bonusy/RockPaperScissors/rps.py
+++ Bonusy/RockPaperScissors/rps.py
@@ -1,10 +1,20 @@
 """
-    Basic RPS game
+    Basic KNP game
 """
 
 # RUNTIME #
 def rps():
-    pass
+    # Winning points necessary to win
+    winning_points = 0
+
+    # Input numbers until user put valid number
+    while winning_points <= 0:
+        winning_points = int(input("Target winning point difference: "))
+
+    
+
+
+
 
 if __name__ == "__main__":
     rps()
