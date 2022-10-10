0f5b27d277ca27f971fafe9e06a1261ccbcab034
Rudolf Klusal <klusik@klusik.cz>
2022-10-10 10:14:13 +0200

    Initial commit -- zadání

---
+++ Bonusy/Pyladies_221011_funkce/funkce.py
@@ -0,0 +1,22 @@
+"""
+    Ukázkový prográmek na Pyladies 11. 10. 2022
+    ===========================================
+
+    Předmět ukázky: FUNKCE
+
+    Cíl ukázky:
+        Vytvořit hru za pomocí funkcí a věcí, co jsme
+        dosud probrali, tedy proměnné a cykly.
+
+    Princip aplikace:
+        Uživatel si v 1. kroku vybere obtížnost (úrovně nechám na vás).
+        Jakmile si vybere správnou obtížnost (kontrolu též přenechám na vás),
+        uživatel začne hrát.
+
+        Samotná hra tkví v uhádnutí počítačem "myšleného" čísla (počítač nemyslí, ale
+        víte, jak to myslím :-D), počítač pak radí, jak se trefit (přidej, uber etc.)
+
+        Výsledkem hry je počet špatných odhadů. Později hru můžeme vylepšit třeba
+        o tabulku vítězů a podobné věci, se současnými znalostmi je toto pouze první krok.
+"""
+
