1f19cf47912d9ce87b6ad0d952da100e892e09bc
Rudolf Klusal <klusik@klusik.cz>
2022-10-11 20:09:31 +0200

    Stav po lekci

---
+++ Bonusy/Pyladies_221011_funkce/podivnypodil.py
@@ -0,0 +1,12 @@
+""" Podivný podíl
+    
+    Chci funkci, která vrátí podíl dvou zadaných čísel (přijme je jako argumenty)
+    a vrátí mi jejich podíl, pokud to jde (nechci dělit nulou), ale pokud je podíl
+    tutěch dvou čísel větší 10, už to stejně vrátí 10.
+    
+    Např. pro čísla 100 a 5 to vrátí 10. Pro čísla 5 a 2 to vrátí 2.5.
+    
+    
+    Rychlé zadání na závěr: Funkce, která přijme dva argumenty, první vypíše, druhý vrátí.
+    
+    """
