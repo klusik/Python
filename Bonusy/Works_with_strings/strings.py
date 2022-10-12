6408a9737ba9341088aed3124960d16f901509e2
Rudolf Klusal <klusik@klusik.cz>
2022-10-12 11:26:36 +0200

    Initial functions & classes set

--- Bonusy/Works_with_strings/strings.py
+++ Bonusy/Works_with_strings/strings.py
@@ -1,5 +1,22 @@
 """
     Use text: https://www.gutenberg.org/files/1342/1342-0.txt
 
-    
+
 """
+
+# CLASSES #
+class Word:
+    pass
+
+
+class Text:
+    pass
+
+
+# RUNTIME #
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
