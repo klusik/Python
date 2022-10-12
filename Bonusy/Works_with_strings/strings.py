ebfefae0f38970f8b6cdb8f76389b9f2534c6494
Rudolf Klusal <klusik@klusik.cz>
2022-10-12 12:02:48 +0200

    Reading a remote file

--- Bonusy/Works_with_strings/strings.py
+++ Bonusy/Works_with_strings/strings.py
@@ -3,6 +3,8 @@
 
 
 """
+# IMPORTS #
+import urllib.request
 
 # CLASSES #
 class Word:
@@ -10,12 +12,30 @@ class Word:
 
 
 class Text:
-    pass
+    def __init__(self):
+        self.path = "https://www.gutenberg.org/files/1342/1342-0.txt"
+        self.text = str()
+
+        self.read_file()
+
+    def read_file(self):
+        file = urllib.request.urlopen(self.path)
+        self.text = file.read()
+
+    def print_file(self):
+        print(self.text)
+
+    def process_file(self):
+        pass
+
+
+
 
 
 # RUNTIME #
 def main():
-    pass
+    text = Text()
+    text.process_file()
 
 
 if __name__ == "__main__":
