5ff204a7046f0c59a977c9f45381c8f599642bce
Rudolf Klusal <klusik@klusik.cz>
2022-10-14 10:52:09 +0200

    Tasks defined

--- Bonusy/GenerateSAPHours/saphours.py
+++ Bonusy/GenerateSAPHours/saphours.py
@@ -3,23 +3,62 @@
     but for default distribution, it should work.
 """
 # IMPORTS #
+import datetime
 
 # CLASSES #
 class Config:
     """ This class just handles configuration """
-    
-    # Configuration
+
+    # Configuration file
     config_file = "config.ini"
 
-    def __init__(self):
-        pass
+    # Used SAP numbers
+    saves = "sap_numbers.txt"
 
-    def get_config_file(self):
-        return self.config_file
+    @staticmethod
+    def get_config_file():
+        return Config.config_file
+
+    @staticmethod
+    def get_saves_file():
+        return Config.saves
+
+
+class Calendar:
+    """ Handles calendar """
+    def __init__(self,
+                 month = None):
+        """ If month is not set up, uses actual month """
+
+        if not month:
+            # Set up an actual month
+            month = datetime.datetime.now().month
+
+        print(month)
 
 # RUNTIME #
 def main():
-    pass
+    """ Main runtime """
+
+    ## Main workflow ##
+
+    # Load previously saved numbers
+
+    # Ask user if do distribution for current month or different
+
+    # Ask user how many days ommit (vacation, public holiday) in given month
+    # Or better solution, which day numbers to omit (e.g. 17 for November)
+
+    # Ask user how many hours to distribute
+
+    # Load calendar for that month
+
+    # Compute for all remanining (not holidays/vacation) workdays
+
+    # Generate CSV (could be easily opened in Excel or something and copied to SAP)
+    # Experiment with text file, values divided by \t or something like that
+
+
 
 if __name__ == "__main__":
     main()
