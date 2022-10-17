245e22ce7ac8a9fd3b88377f399a806d5be0da21
Rudolf Klusal <klusik@klusik.cz>
2022-10-17 11:44:45 +0200

    Meh

--- Bonusy/Klikovac/klikovac.py
+++ Bonusy/Klikovac/klikovac.py
@@ -4,10 +4,21 @@
 """
 
 # IMPORTS #
+import tkinter
+import tkinter.ttk
+
+# CLASSES #
+class Pushuper(tkinter.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
 
 # RUNTIME #
-def main():
-    pass
 
 if __name__ == "__main__":
-    main()
+    main_window = Pushuper()
+
+    main_window.master.title("Pushups for MathSessions")
+    main_window.master.maxsize(400, 200)
+    main_window.master.minsize(400, 200)
+    main_window.mainloop()
