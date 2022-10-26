7c94805e4a7be6fe5d6ac8d908f76c430ab9e1d7
Rudolf Klusal (z0119174) <rudolf.klusal@zf.com>
2022-10-26 09:49:51 +0200

    Working, but slow

--- Bonusy/PrimeFactorizer/prime_factorizer.py
+++ Bonusy/PrimeFactorizer/prime_factorizer.py
@@ -12,9 +12,9 @@
     Author: klusik@klusik.cz
 """
 
-
 # IMPORTS #
 import math
+
 
 # FUNCTIONS #
 def is_prime(number) -> bool:
@@ -32,15 +32,71 @@ def is_prime(number) -> bool:
 
     return True
 
+def generate_primes_to_number(input_number) ->list:
+    """ Generates a list of usable primes to input_number / 2 """
+    list_of_primes = [2]
+
+    for number in range(3, math.floor(input_number / 2 +1), 2):
+        if not(input_number % number):
+            list_of_primes.append(number)
+
+    return list_of_primes
+
+def factorize_number(input_number) -> str:
+    """ Generates factorization """
+
+    # Get all primes
+    primes = generate_primes_to_number(input_number)
+
+    # Result placeholder
+    factorization = ""
+
+    # Counts of divisions
+    divisions = dict()
+
+    # Divide the input_number periodically, until possible by every prime,
+    # track counts of divisions and generate a string accordingly
+
+    for prime in primes:
+        while True:
+            if not (input_number % prime):
+                # Divisible, add to divisions
+                try:
+                    # If exist
+                    divisions[prime] += 1
+                except KeyError:
+                    # not exist
+                    divisions[prime] = 1
+
+                # Do division
+                input_number /= prime
+            else:
+                # Not divisible, next prime
+                break
+
+    # Compile factorization
+    for prime in divisions:
+        if divisions[prime] == 1:
+            factorization += f"({prime})"
+        else:
+            factorization += f"({prime}**{divisions[prime]})"
+
+    # Return result
+    return factorization
+
 
 def prime_factors(input_number) -> str:
     if not is_prime(input_number):
-        pass
+        return factorize_number(input_number)
     else:
-        return(f"({input_number})")
+        return (f"({input_number})")
+
 
 # RUNTIME #
 if __name__ == "__main__":
-    number = int(input("Enter the number: "))
 
-    print(prime_factors(number))
+    try:
+        number = int(input("Enter the number: "))
+        print(prime_factors(number))
+    except KeyboardInterrupt:
+        print("Bye :-)")
