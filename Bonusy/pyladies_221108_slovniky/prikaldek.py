09a7699624d51cf855388a21c8fc8d53f5d00f36
Rudolf Klusal <klusik@klusik.cz>
2022-11-08 19:46:45 +0100

    Pocitaclo psu

---
+++ Bonusy/pyladies_221108_slovniky/prikaldek.py
@@ -0,0 +1,8 @@
+"""
+    Chci list (seznam) nějakých uživatelů, uživatelé budou slovníky.
+    Uživatelé budou mít jména, příjmení a počet psů, které mají doma.
+
+    Na konci budu chtít nějak hezky vypsat ve smyslu
+    "uživatel X má doma Y psů"
+    nebo "uživatel F doma nemá bohužel žádného psa." atd.
+"""
