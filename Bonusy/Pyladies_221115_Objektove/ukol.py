51ed0eeee58fe7cf6ff8e838a0b5be69d7ba395f
Rudolf Klusal <klusik@klusik.cz>
2022-11-15 20:08:25 +0100

    Vyřešený úkol

--- Bonusy/Pyladies_221115_Objektove/ukol.py
+++ Bonusy/Pyladies_221115_Objektove/ukol.py
@@ -20,3 +20,50 @@
 
     Po zadání všech čísel (od uživatele) se zavolá vypisovací metoda na výpis.
 """
+
+class Hodnoty:
+    """ Tuta třída bude obsahovat hodnoty a metody k tomu určené """
+    seznam = []
+
+    def pridej_hodnotu(self, hodnota):
+        """ Přidá hodnotu do seznamu """
+        self.seznam.append(hodnota)
+
+    def smaz_seznam(self):
+        """ Smaže celý seznam """
+        self.seznam.clear()
+
+    def vypis_hodnoty(self):
+        """ Vypise hezky hodnoty pod sebe a očíslovaně """
+
+        if self.seznam:
+            for poradi, hodnota in enumerate(self.seznam):
+                print(f"{poradi + 1}. {hodnota}")
+        else:
+            print("Tento seznam je prázdný.")
+# Zde vytvoříme seznam hodnot na základě třídy Hodnoty() (vytváříme objekt)
+seznam_hodnot = Hodnoty()
+dalsi_seznam = Hodnoty()
+a_jeste_jeden = Hodnoty()
+
+
+while True:
+    """ Tohle je smyčka, která to celé obsluhuje """
+    # Zeptáme se uživatele na hodnotu
+    hodnota = int(input("Zadej hodnotu: "))
+
+    if hodnota == 0:
+        # Ukončení po zadání nuly
+        break
+    else:
+        # Pokud zadal cokoliv jiného než nulu
+        seznam_hodnot.pridej_hodnotu(hodnota)
+
+
+# Až doběhne zadávání, vypíšeme
+seznam_hodnot.vypis_hodnoty()
+
+# Smažeme všechny hodnoty
+seznam_hodnot.smaz_seznam()
+seznam_hodnot.vypis_hodnoty()
+
