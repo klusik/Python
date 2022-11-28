89a269ba38a6a4c52940a1dd31c7b7882756cd80
Rudolf Klusal <klusik@klusik.cz>
2022-11-28 12:34:32 +0100

    Druhý soubor hotov

--- Bonusy/Pyladies_221115_Objektove/properties_reseni.py
+++ Bonusy/Pyladies_221115_Objektove/properties_reseni.py
@@ -1,5 +1,7 @@
 """
     Kód vysvětlující properties v objektovém programování.
+
+    Změny jsou okomentované :-)
 """
 
 
@@ -17,8 +19,24 @@ class Uzivatel:
         # Nastavíme hodnoty instance:
         self.jmeno = jmeno
         self.prijmeni = prijmeni
-        self.email = f"{jmeno}.{prijmeni}@klusik.cz"
+        # self.email = f"{jmeno}.{prijmeni}@klusik.cz" # Proč je zakomentováno se dozvíte níže
 
+    # Nejdříve k tomu emailu, aby se měnil v momentě, co potřebujeme
+    # Jako první krok vyrobíme metodu, která se bude jmenovat tak, jako
+    # hodnota (email), kterou chceme dostat.
+    # Kdybychom však měli metodu, v programu bychom to museli volat
+    # třeba jako pepa.email() ale chceme se odkazovat pouze na
+    # pepa.email (bez závorek).
+    #
+    # K tomu slouží právě @property dekorátor! A samozřejmě
+    # pak ani nebudeme potřebovat atribut self.email výše.
+    @property
+    def email(self):
+        return f"{self.jmeno}.{self.prijmeni}@klusik.cz"
+
+    # Stejným způsobem můžeme upravit i metodu cele_jmeno, v kodu pak
+    # se neodkazujeme jako pepa.cele_jmeno(), ale jen pepa.cele_jmeno.
+    @property
     def cele_jmeno(self):
         """
             Vrátí celé jméno uživatele
@@ -26,6 +44,19 @@ class Uzivatel:
         """
         return f"{self.jmeno} {self.prijmeni}"
 
+    @cele_jmeno.setter
+    def cele_jmeno(self, cele_jmeno:str):
+        """ Ano, vyrobím metodu, která se jmenuje stejně jako předchozí,
+            jen ji dám dekorátor se jménem té metody """
+
+        # Tím, že udělám "setter," říkám, že metoda nastavuje nějaké hodnoty.
+        jmeno, prijmeni = cele_jmeno.split()
+        self.jmeno = jmeno
+        self.prijmeni = prijmeni
+
+        # Program tedy nastaví tyto hodnoty a tím, že je to "property" se
+        # pak všechno dynamicky počítá při volání a hodnoty nezůstávají na
+        # starších verzích hodnot :-)
 
 # Vyrobíme tedy uživatele, který bude mít nějaké jméno:
 
@@ -33,7 +64,7 @@ pepa = Uzivatel("Josef", "Dvorak")
 
 # Zobrazíme si informace o uživateli
 print(f"Křestní jméno uživatele: {pepa.jmeno}")
-print(f"Celé jméno uživatele: {pepa.cele_jmeno()}")
+print(f"Celé jméno uživatele: {pepa.cele_jmeno}")
 print(f"Email uživatele: {pepa.email}")
 
 # Teď změníme jméno uživatele:
@@ -41,8 +72,13 @@ pepa.jmeno = "Jiri"
 
 # Vypíšeme zase hodnoty
 print(f"Křestní jméno uživatele: {pepa.jmeno}") # Vidíme, že jméno to změnilo
-print(f"Celé jméno uživatele: {pepa.cele_jmeno()}") # Vidíme, že metoda se na to jméno odkazuje a změní to taky
+print(f"Celé jméno uživatele: {pepa.cele_jmeno}") # Vidíme, že metoda se na to jméno odkazuje a změní to taky
 print(f"Email uživatele: {pepa.email}") # Tady ale vidíme, že email se vyrobil už před tím a tohle ho nezměnilo
 
-# A vidíme, že "to není ono" :-) Jak to tedy udělat, aby to 'bylo ono'?
-# To se dozvíte v souboru properties_reseni.py
+# Dále budu chtít třeba použít atribut .cele_jmeno na změnění hodnoty!
+pepa.cele_jmeno = "Rudolf Klusal" # Normálně by to nešlo, ale tím, jak to nastavíme ve třídě, už to půjde
+
+# Nakonec opět zobrazím hodnoty:
+print(f"Křestní jméno uživatele: {pepa.jmeno}") # Vidíme, že jméno to změnilo
+print(f"Celé jméno uživatele: {pepa.cele_jmeno}") # Vidíme, že metoda se na to jméno odkazuje a změní to taky
+print(f"Email uživatele: {pepa.email}") # Tady ale vidíme, že email se vyrobil už před tím a tohle ho nezměnilo
