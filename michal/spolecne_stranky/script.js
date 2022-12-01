b6be35647ff0be76c7e3bb78eff285252d32e0df
kopfrkingl <sykora.michal1@gmail.com>
2022-12-01 23:41:06 +0100

    advent of code 2022
    initial commit

---
+++ michal/spolecne_stranky/script.js
@@ -0,0 +1,7 @@
+// hello world
+console.log('hello world');
+alert('vyhul')
+confirm('gay?')
+city = prompt('kde bydlis?')
+longevity = prompt('jak dlouho se u vas zije?')
+console.log(`city: ${city}; longevity: ${longevity}`)
