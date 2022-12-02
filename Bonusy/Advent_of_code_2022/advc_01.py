197f29aa4f642448bfadbc0b6d44afe377b5f25b
Rudolf Klusal <klusik@klusik.cz>
2022-12-02 09:24:07 +0100

    Solution

---
+++ Bonusy/Advent_of_code_2022/advc_01.py
@@ -0,0 +1,27 @@
+"""
+    Elfové
+"""
+
+# RUNTIME #
+if __name__ == "__main__":
+    # Open file
+    with open("d1t1_input.txt", "r") as f_input:
+        data = f_input.read()
+
+    # Separate by empty lines
+    data_packs = data.split('\n\n')
+
+    # Create list of sums of elven work and select the maximal (for each elf)
+    max_elf = max(list(map(lambda l: (sum(list(map(int, l.split())))), data_packs)))
+    print(max_elf)
+
+
+
+
+
+
+
+
+
+
+
