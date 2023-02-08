3554cd7392926f130aa3366456acc3f13ed031d1
Rudolf Klusal <klusik@klusik.cz>
2023-02-08 14:55:11 +0100

    Normalize output dict frequencies
    Signed-off-by: Rudolf Klusal <klusik@klusik.cz>

--- Bonusy/Random language generator/language.py
+++ Bonusy/Random language generator/language.py
@@ -41,33 +41,50 @@ class Article:
 class Alphabet:
     """ Frequency of characters in common English """
     char_frequency = {
-        'E': 12.02,
-        'T': 9.10,
-        'A': 8.12,
-        'O': 7.68,
-        'I': 7.31,
-        'N': 6.95,
-        'S': 6.28,
-        'R': 6.02,
-        'H': 5.92,
-        'D': 4.32,
-        'L': 3.98,
-        'U': 2.88,
-        'C': 2.71,
-        'M': 2.61,
-        'F': 2.30,
-        'Y': 2.11,
-        'W': 2.09,
-        'G': 2.03,
-        'P': 1.82,
-        'B': 1.49,
-        'V': 1.11,
-        'K': 0.69,
-        'X': 0.17,
-        'Q': 0.11,
-        'J': 0.10,
-        'Z': 0.07,
+        'e': 12.02,
+        't': 9.10,
+        'a': 8.12,
+        'o': 7.68,
+        'i': 7.31,
+        'n': 6.95,
+        's': 6.28,
+        'r': 6.02,
+        'h': 5.92,
+        'd': 4.32,
+        'l': 3.98,
+        'u': 2.88,
+        'c': 2.71,
+        'm': 2.61,
+        'f': 2.30,
+        'y': 2.11,
+        'w': 2.09,
+        'g': 2.03,
+        'p': 1.82,
+        'b': 1.49,
+        'v': 1.11,
+        'k': 0.69,
+        'x': 0.17,
+        'q': 0.11,
+        'j': 0.10,
+        'z': 0.07,
     }
+
+    @classmethod
+    def get_sum_frequency(cls) -> float:
+        return sum(cls.char_frequency.values())
+
+    @classmethod
+    def get_max_frequency(cls) -> float:
+        return max(cls.char_frequency.values())
+
+    @classmethod
+    def get_normalized_frequencies(cls):
+        normalized_chars = cls.char_frequency
+
+        # Ratio would be 1 : max_value from character dict
+        ratio = 1 / cls.get_max_frequency()
+
+        return {letter : value * ratio for letter, value in cls.char_frequency.items()}
 
 
 class Syntax:
@@ -346,6 +363,8 @@ def main():
         print(sentence, end='')
 
     print()
+
+    print(Alphabet.get_normalized_frequencies())
 
 
 if __name__ == "__main__":
