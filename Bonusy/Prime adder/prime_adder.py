d15212c375f693710a7674870c1b013230c55ec1
Rudolf Klusal <klusik@klusik.cz>
2023-02-21 22:26:58 +0100

    Initial commit
    Signed-off-by: Rudolf Klusal <klusik@klusik.cz>

---
+++ Bonusy/Prime adder/prime_adder.py
@@ -0,0 +1,13 @@
+"""
+    Create a prime adder, which can construct any number from input
+    but just only as a sum of unique primes. For example:
+
+    input: 10
+    output: [2, 5, 3]
+
+    input: 11
+    output: [11]
+
+    input: 20
+    output: [2, 5, 13]
+"""
