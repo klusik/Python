bb3ac8d8c2ab9e5da4db75bb30aab38eac3db2db
Rudolf Klusal <klusik@klusik.cz>
2024-02-19 14:11:21 +0100

    Úlohy na opakovací pyladies
    Zadání úloh
    Výrobky

---
+++ Bonusy/Pyladies/Soubory na procvičení/nakupovani_3.py
@@ -0,0 +1,21 @@
+"""
+    Zadání 3:
+
+    Vyrobte porgram, který vytvoří nákupní seznam
+    s cenou sečtenou podle toho, co si uživatel vymyslí.
+
+    Zadávání bude vypadat např. takto:
+
+    Výpis: Máte k dispozici následující výrobky: mléko, mouka, ... (výpis všech).
+    Který výrobek zakoupit?
+
+    uživatel zadá třeba 'mléko'
+
+    program se zeptá: Kolik?
+
+    uživatel zadá třeba 3
+
+    Program si připočte k ceně cenu 3×22 za mléka.
+
+    Na konci prográmek vypíše, kolik Kč bude takový nákup stát.
+"""
---
+++ Bonusy/Pyladies/Soubory na procvičení/nakupovani_4_pokročilé.py
@@ -0,0 +1,13 @@
+"""
+    Zadání 4: (mírně už pokročilé pro ty, kteří jsou už trošku napřed)
+
+    Vytvořte prográmek z úkolu 3, ale bude si pamatovat nejen celkovou cenu,
+    ale kompletně celý nákup, tedy na konci vypíše třeba:
+
+    Budete nakupovat:
+    3krát mléko, celkem 3 × 22 = 66 Kč
+    1krát mouka, celkem 1 × 25 = 25 Kč
+    5krát kaiserka, celkem 5 × 4.5 = 22,5 Kč
+
+    Celková cena nákupu: 113,5 Kč
+"""
