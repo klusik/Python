46a71cf0c0b026793932a42f38335d45eddabaaa
Rudolf Klusal <klusik@klusik.cz>
2024-02-22 06:51:04 +0100

    Basic logika

--- Bonusy/Pyladies/Soubory na procvičení/nakupovani_5_reseni.py
+++ Bonusy/Pyladies/Soubory na procvičení/nakupovani_5_reseni.py
@@ -27,3 +27,72 @@
 
         Celková cena nákupu 115 Kč.
 """
+
+def nacti_seznam():
+    """
+    Přečte soubor a vytvoří datovou strukturu
+    @return: slovník seřazených výrobků
+    """
+    with open('vyrobky.txt', 'r', encoding='utf-8') as f_vyrobky:
+        vyrobky = f_vyrobky.read().split('\n')
+
+
+    slovnik_vystup = dict()
+
+    for vyrobek in vyrobky:
+        vyrobek_jmeno, vyrobek_cena = vyrobek.split()
+        slovnik_vystup[vyrobek_jmeno] = vyrobek_cena
+
+
+    return dict(sorted(slovnik_vystup.items(), key = lambda item: item[1], reverse=True))
+
+def cena_nakupniho_seznamu(nakupni_seznam):
+    """
+    Vypočítá celkovou cenu seznamu
+    @param nakupni_seznam: nákupní seznam
+    @return: float s cenou nákupního seznamu
+    """
+
+    return float(sum(nakupni_seznam.items()[1]))
+
+def main():
+    while True:
+        # Zadání dat od uživatele
+        pocet = int(input("Zadej počet výrobků: "))
+        cena_min = int(input("Zadej minimální cenu: "))
+        cena_max = int(input("Zadej maximální cenu: "))
+
+        if pocet > 0 and (1 <= cena_min < cena_max):
+            # Načtu seřazený slovník výrobků
+            vyrobky = nacti_seznam()
+
+            # Procházím nejdražší výrobky a snažím se vyrobit nákup tak, abych se trefil mezi rozsahy ceny.
+            # Pokud se mi podaří trefit nejdražšími výrobky do ceny, program je hotov.
+            # Pokud cenu "přetáhnu," pak uberu poslední nejdražší výrboek z nákupního seznamu
+            # a zkusím přidat levnější.
+            # Pokud se ani toto nepodaří, uberu i ten levnější a přidám ještě levnější etc.
+
+            nakupni_seznam = dict()
+
+            while True:
+                # Vnitřní smyčka řešící fitnutí výrobků do ceny
+                if len(vyrobky):
+                    # Máme-li ještě nějaké výrobky
+                    if cena_nakupniho_seznamu(nakupni_seznam) > cena_max:
+                        # přehnali jsme hodnotu
+                        # je třeba ze seznamu odstranit poslední přidanou položku
+                        nakupni_seznam.pop()
+
+                        # nadále budeme ignorovat drahý výrobek, odstraníme ho
+                        vyrobky.pop(0)  # odstranění nejdražšího výrobku z výrobků
+                        continue  # vnitřní smyčka běží dál
+                else:
+                    # Nemáme výrobky
+                    break
+
+
+
+
+
+if __name__ == "__main__":
+    main()
