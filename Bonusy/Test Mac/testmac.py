e0076cd79ea173acf3e6514aede7722f2dfcc367
Rudolf Klusal <klusik@klusik.cz>
2024-11-09 15:53:05 +0100

    New mac
    So I`m testing out

---
+++ Bonusy/Test Mac/testmac.py
@@ -0,0 +1,12 @@
+"""
+    Test of Python on Mac (it should work everywhere though)
+
+"""
+
+class App:
+    def __init__(self):
+        print("Welcome to this app :-)")
+
+
+if __name__ == "__main__":
+    app = App()
