ed995cbc3ef46f3bd50a2b659abe092a32020f5b
Rudolf Klusal <klusik@klusik.cz>
2024-11-09 15:54:58 +0100

    Just a test func

--- Bonusy/Test Mac/testmac.py
+++ Bonusy/Test Mac/testmac.py
@@ -7,6 +7,10 @@ class App:
     def __init__(self):
         print("Welcome to this app :-)")
 
+    def test_func(self):
+        """ Just a test func
+        """
+
 
 if __name__ == "__main__":
     app = App()
